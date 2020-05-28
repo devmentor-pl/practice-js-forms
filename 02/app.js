d7c6421ab4efbca6d3ae1e3ab42132ea67f3e81b
Dawid <dwilk92@o2.pl>
2020-05-28 14:17:09 +0200

    zad 2 gotowe

--- 02/app.js
+++ 02/app.js
@@ -1,13 +1,63 @@
 const formEl = document.querySelector('form');
-console.log(formEl);
+//console.log(formEl);
+const labelEl = document.querySelectorAll('label');
+//console.log(labelEl)
+//const ulEl = document.createElement('ul');
+//ulEl.classList.add('errors');
+//formEl.appendChild(ulEl)
+
 formEl.addEventListener('submit', function(e)
 {
-    e.preventDefault();
+   e.preventDefault();
+
+    const emailEl= e.target.elements.login.value; // tutaj zawsze przedostatnia część po kropce to jest name w HTML
+    const password1 = e.target.elements.pass1.value;
+    const password2 = e.target.elements.pass2.value;
+    const confirm = e.target.elements['accept'];
+    const errorsArr = [];
+   // const a = document.getElementById('formAccept')
+
+   //console.log(a)
+    if(!emailEl.includes('@')){
+        errorsArr.push(e.target.elements.login);
+        //e.target.elements.login.style.border = '1px solid red'
+    }
+    //else {
+        //e.target.elements.login.style.border = '';
+    //}
+
+    if(password1 !== password2 || password1.length < 6)
+    {
+        errorsArr.push(e.target.elements.pass1, e.target.elements.pass2);
+        //e.target.elements.pass1.style.border = '1px solid red';
+        //e.target.elements.pass2.style.border = '1px solid red';
+        //console.log(password2.length)
+    }
+       //else {
+          // e.target.elements.pass1.style.border = '';
+         //  e.target.elements.pass2.style.border = '';
+       //}
+    if(!confirm.checked){
+        errorsArr.push(confirm);
+       //a.style.border = '5px solid red'
+       alert('musisz zaakceptować regulamin');
+    }
+        
+    //console.log(errorsArr)
+    if(errorsArr.length > 0)
+    {
+       //console.log('aa')
+       errorsArr.forEach(function(e)
+       {
+           //console.log(e)
+           e.style.border = '1px solid red';
+       })
+        
+    }
+    else{
+         console.log('done');
+         
+    }
+
 })
 
-const handleChange = function(e) {
-        console.log(        e.target.name,        e.target.value,    );
-     } 
-for(const el of formEl.elements)
- {    el.addEventListener('change', handleChange);   
-  }
