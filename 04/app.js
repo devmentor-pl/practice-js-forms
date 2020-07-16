193d955d82e4f4143f360ee289ac1b7e17d46344
Martin Gawlyta <martin.guide@gmail.com>
2020-07-16 16:03:42 +0200

    four down

--- 04/app.js
+++ 04/app.js
@@ -2,20 +2,19 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const boxElement = document.querySelector('.box');
-    setBoxShadow(boxElement, '#000000');
+    setBoxShadow(boxElement, '#00000');
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
     const colorInRGBA = `rgba(
-        ${getChannelColor(colorInHex, 'red')}, 
-        ${getChannelColor(colorInHex, 'green')}, 
-        ${getChannelColor(colorInHex, 'blue')}, 
+        ${getChannelColor(colorInHex, 'red')},
+        ${getChannelColor(colorInHex, 'green')},
+        ${getChannelColor(colorInHex, 'blue')},
         ${opacity}
     )`;
 
     element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
 }
-
 
 function getChannelColor(colorInHex, channelName) {
     let start;
@@ -34,7 +33,25 @@ function getChannelColor(colorInHex, channelName) {
     const channelColorHex = colorInHex.substr(start, 2);
     const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+    return channelColorDec;
 }
 
 
+// // SOLUTION
+
+function getShadowValues() {
+  // get box element
+  const boxElement = document.querySelector('.box');
+  // color
+  const color = document.querySelector('input[name="color"]').value;
+  // opacity
+  const opacity = document.querySelector('input[name="opacity"]').value;
+
+  // change color and opacity of box shadow
+  setBoxShadow(boxElement, color, opacity*0.01);
+};
+
+// attach selector to the elements
+document.querySelectorAll('.panel > input').forEach(item => {
+  item.addEventListener('change', getShadowValues);
+})
