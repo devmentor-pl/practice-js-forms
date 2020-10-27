70d23178e198773327b2f87a953fc7f2c39829cb
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-27 07:32:54 +0100

    Change box shadow and opacity in Task 04

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,40 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+    const selectorPanel = document.querySelector('.panel');
+
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
+
+    selectorPanel.addEventListener('mousemove', changeBox);
+    selectorPanel.addEventListener('change', changeBox);
 }
+
+function changeBox(e) {
+
+    const colorInput = document.querySelector('input[name="color"]');
+    const shadowInput = document.querySelector('input[name="opacity"]');
+    const boxElement = document.querySelector('.box');
+
+    const isMouseMoveEvent = e.type === 'mousemove';
+    const isMouseLeftButtonPress = e.buttons === 1;
+    if (
+        isMouseMoveEvent && isMouseLeftButtonPress || !isMouseMoveEvent
+    ) {
+
+
+        const value = shadowInput.value;
+        const opacity = value / 100;
+
+        const colorInHex = colorInput.value;
+        console.log(colorInHex);
+
+        setBoxShadow(boxElement, colorInHex, opacity)
+    }
+}
+
+
+
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
     const colorInRGBA = `rgba(
@@ -19,7 +50,7 @@ function setBoxShadow(element, colorInHex, opacity = 1) {
 
 function getChannelColor(colorInHex, channelName) {
     let start;
-    switch(channelName) {
+    switch (channelName) {
         case 'red':
             start = 1;
             break;
@@ -34,7 +65,5 @@ function getChannelColor(colorInHex, channelName) {
     const channelColorHex = colorInHex.substr(start, 2);
     const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+    return channelColorDec;
 }
-
-
