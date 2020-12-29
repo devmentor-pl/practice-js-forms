2096da96e413b1f84f887d46acf7ee8617241a82
Ewa Skórska <ewaskorska94@gmail.com>
2020-12-29 15:38:07 +0000

    Handle color and opacity picking

--- 04/app.js
+++ 04/app.js
@@ -3,6 +3,26 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
+    const colorPickerEl = document.querySelector('[name="color"]');
+    colorPickerEl.addEventListener('mousemove', changeColor);
+    colorPickerEl.addEventListener('change', changeColor);
+
+    const rangeEl = document.querySelector('[name="opacity"');
+    rangeEl.addEventListener('input', changeOpacity);
+}
+
+function changeOpacity(event) {
+    const boxElement = document.querySelector('.box');
+    const opacityRange = event.target.value;
+    const currentColor = document.querySelector('[name="color"]').value;
+    setBoxShadow(boxElement, currentColor, opacityRange/100);
+}
+
+function changeColor(event) {
+    const boxElement = document.querySelector('.box');
+    const color = event.target.value;
+    const curentOpacity = document.querySelector('[name="opacity"').value;
+    setBoxShadow(boxElement, color, curentOpacity);
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
