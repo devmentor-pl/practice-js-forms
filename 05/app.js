95f4e666cbf696619dc6652ded34ef1fd9b260f6
Ewa Skórska <ewaskorska94@gmail.com>
2020-12-30 17:52:47 +0000

    Fix error messages

--- 05/app.js
+++ 05/app.js
@@ -1,13 +1,14 @@
 const REQUIRED_ERROR_MESSAGE = "To pole jest wymagane";
 const NUMBER_ERROR_MESSAGE = "To pole jest wymagane";
+const POSITIVE_NUMBER_ERROR_MESSAGE = "Podana wartość powinna być dodatnia";
 const ZIP_CODE_ERROR_MESSAGE = "Wprowadź poprawny kod pocztowy np.\"52-690\"";
 
-const errorsMessages = [];
+const errorMessages = [];
 
 document.addEventListener('DOMContentLoaded', init);
 function init() {
-    console.log('.init()');
-    
+    // console.log('.init()');
+
     const formEl = document.querySelector('form');
     formEl.noValidate = true;
     formEl.addEventListener('submit', validateAll);
@@ -18,9 +19,20 @@ function validateAll(event) {
     let errors = [];
     const formEl = event.currentTarget;
     isRequired(formEl, errors);
-    isNumber(formEl, errors);
+    isPositiveNumber(formEl, errors);
     isMatching(formEl, errors);
     colorErrors(errors);
+    addToMessages();
+}
+
+function addToMessages() {
+    errorMessages.forEach(function(message) {
+        const liEl = document.createElement('li');
+        liEl.innerText = message;
+        document.querySelector('.messages').appendChild(liEl);
+        //wiem, że to jest brzydko, bo zamiast wszystko na raz wcisnąć to ciągle się odwołuję, ale już nie mam pomysłu. (albo sił
+    })
+    errorMessages.length = 0;
 }
 
 function colorErrors(errors) {
@@ -38,7 +50,7 @@ function isRequired(formEl, errors) {
     requiredInputs.forEach(function(element) {
         if(element.value === "") {
             errors.push(element.name);
-            console.log(element.name + " - " + REQUIRED_ERROR_MESSAGE);
+            errorMessages.push(element.name + " - " + REQUIRED_ERROR_MESSAGE);
         }
         else {
             markOK(element);
@@ -46,14 +58,18 @@ function isRequired(formEl, errors) {
     })
 }
 
-function isNumber(formEl, errors) {
+function isPositiveNumber(formEl, errors) {
     const numberInputs = formEl.querySelectorAll('[type="number"]');
     numberInputs.forEach(function(element) {
         if(isNaN(element.value)) {
             errors.push(element.name);
-            console.log(element.name + " - " + NUMBER_ERROR_MESSAGE);
+            errorMessages.push(element.name + " - " + NUMBER_ERROR_MESSAGE);
         }
-        else if(element.value !== "" && !isNaN(element.value)) {
+        else if(element.value <= 0) {
+            errors.push(element.name);
+            errorMessages.push(element.name + " - " + POSITIVE_NUMBER_ERROR_MESSAGE);
+        }
+        if(element.value !== "") {
             markOK(element);
         }
     })
