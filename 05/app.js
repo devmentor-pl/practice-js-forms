0e7df721f5972208fcc2456cf82475d83c430bca
Ewa Skórska <ewaskorska94@gmail.com>
2020-12-31 14:31:14 +0000

    Refactor code

--- 05/app.js
+++ 05/app.js
@@ -3,8 +3,6 @@ const NUMBER_ERROR_MESSAGE = "To pole jest wymagane";
 const POSITIVE_NUMBER_ERROR_MESSAGE = "Podana wartość powinna być dodatnia";
 const ZIP_CODE_ERROR_MESSAGE = "Wprowadź poprawny kod pocztowy np.\"52-690\"";
 
-const errorMessages = [];
-
 document.addEventListener('DOMContentLoaded', init);
 function init() {
     // console.log('.init()');
@@ -18,44 +16,44 @@ function validateAll(event) {
     event.preventDefault();
     let errors = [];
     const formEl = event.currentTarget;
-    isRequired(formEl, errors);
-    isPositiveNumber(formEl, errors);
-    isMatching(formEl, errors);
-    colorErrors(errors);
-    addToMessages();
+    markAllOK(formEl);
+    validateRequiredFields(formEl.querySelectorAll('[required]'), errors);
+    showMessages(errors);
 }
 
-function addToMessages() {
-    errorMessages.forEach(function(message) {
+function markAllOK(formEl) {
+    const allOK = formEl.querySelectorAll('input, select');
+    allOK.forEach((element) => {
+        if(element.value !== "") {
+            element.style.border = '2px solid #03a546';
+        }
+    })
+}
+
+function showMessages(errors) {
+    const ulEl = document.querySelector('.messages');
+    errors.forEach(function(err) {
+        const {element, message} = err;
         const liEl = document.createElement('li');
-        liEl.innerText = message;
-        document.querySelector('.messages').appendChild(liEl);
-        //wiem, że to jest brzydko, bo zamiast wszystko na raz wcisnąć to ciągle się odwołuję, ale już nie mam pomysłu. (albo sił
+        liEl.innerText = `${element.name} - ${message}` ;
+        element.style.border = '2px solid red';
+        ulEl.appendChild(liEl);
     })
-    errorMessages.length = 0;
 }
 
-function colorErrors(errors) {
-    errors.forEach(function(name) {
-        element = document.querySelector(`[name=${name}`);
-        if(element) {
-            element.style.border = '2px solid red';
-        }
-    });
-    errors.length = 0;
-}
-
-function isRequired(formEl, errors) {
-    const requiredInputs = formEl.querySelectorAll('[required]');
-    requiredInputs.forEach(function(element) {
-        if(element.value === "") {
-            errors.push(element.name);
-            errorMessages.push(element.name + " - " + REQUIRED_ERROR_MESSAGE);
-        }
-        else {
-            markOK(element);
+function validateRequiredFields(fieldsList, errors) {
+    fieldsList.forEach(function(element) {
+        if(isEmpty(element.value)) {
+            errors.push({
+                element, //element: element,
+                message: REQUIRED_ERROR_MESSAGE,
+            });
         }
     })
+}
+
+function isEmpty(value) {
+    return value === "";
 }
 
 function isPositiveNumber(formEl, errors) {
@@ -73,21 +71,4 @@ function isPositiveNumber(formEl, errors) {
             markOK(element);
         }
     })
-}
-
-function isMatching(formEl, errors) {
-    const patternInputs = formEl.querySelectorAll('[pattern]');
-    patternInputs.forEach(function(element) {
-        if(element.value !== "" && !element.value.match(element.pattern)) {
-            errors.push(element.name);
-            console.log(element.name + " - " + ZIP_CODE_ERROR_MESSAGE);
-        }
-        else if(element.value.match(element.pattern)) {
-            markOK(element);
-        }
-    })
-}
-
-function markOK(element) {
-    element.style.border = '2px solid #03a546';
 }
