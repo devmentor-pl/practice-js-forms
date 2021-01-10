2486d4a91517ed1f85494859cdd2d8020a1b0985
Marek Mitko <mitko.email@gmail.com>
2021-01-10 23:39:36 +0100

    ex. 4 is done

--- 04/app.js
+++ 04/app.js
@@ -4,22 +4,27 @@ function init() {
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
 
-    const colorPickerEl = document.querySelector('[type="color"]');
-    const eventPicker = colorPickerEl.addEventListener('change', getColor);
-  //... jak to wywołać  
-    console.log(eventPicker, boxElement, getColor)
-    changeColor(boxElement, getColor);
-}
-function changeColor(boxElement, colorInHex) {
-        setBoxShadow(boxElement, colorInHex);
-}
+    
+    const pickerColorEl = document.querySelector('[type="color"]');
+    if(pickerColorEl.addEventListener) {
+        pickerColorEl.addEventListener('change', function(event) {      //?dev troche miałem z tym problem jak chciałem tą funkcję zrobić poza tym ddEventListener//
+            const pickerColorValue = event.target.value;                 //*see rozumiem że trzeba by tu użyć jakkieś metody bind .... czy jak to inaczej ugryźć? 
+            setBoxShadow(boxElement, pickerColorValue);
+        });
+    };
+    const rangeEl = document.querySelector('[type="range"]');           //*see no pewnie to powtórzenie jest bezsensu ale już nie myślę - do przemyślenia 
+    rangeEl.addEventListener('change', function opacityValue(event) {
+        const opacity = (event.target.value)/100;
+        setBoxShadow(boxElement, pickerColorEl.value, opacity);
+    });
+    rangeEl.addEventListener('mousemove', function(event) {
+        const opacity = (event.target.value)/100;
+        setBoxShadow(boxElement, pickerColorEl.value, opacity);
+    });
 
-function getColor(event) {
-    const colorInHex = event.target.value;
-    return colorInHex;
-}
+    }; 
 
-function setBoxShadow(element, colorInHex, opacity = 1) {
+function setBoxShadow(element, colorInHex, opacity = 1) {   //?dev I tak mi trochę tu coś nie gra, bo jak ustawie suwak opacity na watość <1 a potem zmienie kolor, to i tak początkowo opacity tego nowego koloru jest na max tzn = 1. I chyba trochę nie wiem jak to uchwycić. 
     const colorInRGBA = `rgba(
         ${getChannelColor(colorInHex, 'red')}, 
         ${getChannelColor(colorInHex, 'green')}, 
