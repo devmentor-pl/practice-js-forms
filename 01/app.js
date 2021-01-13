aeaa210c763d0ab42dc37e8a9689687f9d7e2b72
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-13 15:06:02 +0100

    Zad 01 added class

--- 01/app.js
+++ 01/app.js
@@ -8,6 +8,7 @@ form.addEventListener('submit', function (e) {
     const newLi = document.createElement('li');
     const newText = document.createTextNode(firstName + ' ' + lastName);
 
+    newLi.className = 'users-list__person';
     newLi.appendChild(newText);
     document.querySelector('.users-list').appendChild(newLi);
 })
