b114aa337215bf13a6409cd1c55a73e40cd4122e
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-27 01:35:37 +0100

    Zad 05

--- 05/app.js
+++ 05/app.js
@@ -1,5 +1,7 @@
 const ulList = document.querySelector('.messages');
 const form = document.querySelector('form');
+const option = document.querySelectorAll('option');
+
 form.addEventListener('submit', checkData);
 
 function checkData(e) {
@@ -11,22 +13,86 @@ function checkData(e) {
     const flatNumber = e.target.elements.flatNumber.value;
     const zip = e.target.elements.zip.value;
     const city = e.target.elements.city.value;
-    const voivodeship = e.target.elements.voivodeship;
+    const voivodeship = e.target.elements.voivodeship.value;
+
     const errors = [];
 
-    if (e.target.elements.length === 0) {
-        errors.push('To pole jest wymagane')
-    }
-    if (!voivodeship.selected) {
-        errors.push('Zaznacz województwo');
+
+    // const fields = e.target.elements;
+    // for(let i=0; i<fields.length; i++) {
+    //     const f = fields[i];
+
+    //     const label = f.previousElementSibling;
+    //     if(label && label.tagName === 'LABEL') {
+    //         label.style.color = '';
+    //     }
+
+    //     if(f.required && f.value === '') {
+    //         label.style.color = 'red';
+    //         errors.push('Pole '+ label.innerText+'musi być wypełnione!');
+    //     }
+
+    //     const num = Number(f.value);
+    //     if(f.type === 'number' && (Number.isNaN( num ) || num <= 0)) {
+    //         label.style.color = 'red';
+    //         errors.push('Pole '+ label.innerText+ 'musi być liczbą!');
+    //     }
+    // }
+
+    if (firstName.length === 0) {
+        errors.push('Wpisz imię');
     }
 
+    if (lastName === '') {
+        errors.push('Wpisz nazwisko');
+    }
+
+    if (houseNumber === '') {
+        errors.push('Wpisz numer');
+        if (houseNumber !== Number) {
+            errors.push('Pole musi być liczbą');
+        }
+    }
+
+    if (flatNumber === '') {
+        errors.push('Wpisz numer');
+        if (flatNumber !== Number) {
+            errors.push('Pole musi być liczbą');
+        }
+    }
+
+    if (zip === '') {
+        errors.push('Wpisz kod pocztowy');
+        if (houseNumber !== Number) {
+            errors.push('Kod musi być liczbą');
+        }
+    }
+
+    if (city === '') {
+        errors.push('Wpisz miasto');
+    }
+
+    // if (typeof option != 'string') {
+    //     errors.push('Wybierz województwo');
+    // }
+
+    if (option.innerText === '') {
+        errors.push('Wybierz województwo');
+    }
+
+
+    ulList.innerHTML = '';
     if (errors.length > 0) {
-        // e.preventDefault();
         errors.forEach(function (err) {
             const newLi = document.createElement('li');
             newLi.innerText = err;
             ulList.appendChild(newLi);
         });
+    }
+
+    if (errors.length > 0) {
+        const input = document.getElementsByTagName('input');
+        const label = input.previousElementSibling;
+        label.style.color = 'red';
     }
 }
