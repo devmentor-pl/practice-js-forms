47bd0115944679decaa90c0a94eaff5741bc82d0
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2021-02-21 21:29:32 +0100

    Zadanie 1 poprawione

--- 01/app.js
+++ 01/app.js
@@ -1,12 +1,14 @@
-const form = document.querySelector('form')
+const formEl = document.querySelector('form');
+const ulEl = document.querySelector('.users-list');
 
-form.addEventListener('submit', e => {
+formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-  const ulElement = document.querySelector('.users-list')
-  const firstName = e.target.elements[0].value;
-  const lastName = e.target.elements[1].value;
-  const liElement = document.createElement('li')
-  liElement.innerText = `${firstName} ${lastName}`;
-  liElement.classList.add('users-list__person')
-  ulElement.appendChild(liElement)
-})
+  const firstName = e.target.elements.firstName;
+  const lastName = e.target.elements.lastName;
+  const liEl = document.createElement('li');
+  liEl.classList.add('users-list__person');
+  liEl.innerText = `${firstName.value} ${lastName.value}`;
+  ulEl.appendChild(liEl);
+  firstName = '';
+  lastName = '';
+});
