20f276513c481dd86aadde38f0c986be3519d25d
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2021-02-22 17:22:52 +0000

    zadanie nr 2 zrobione

---
+++ 02/app.js
@@ -0,0 +1,72 @@
+// const form = document.querySelector('form');
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const elements = e.target.elements;
+//   const loginVal = elements.login.value;
+//   const pass1Val = elements.pass1.value;
+//   const pass2Val = elements.pass2.value;
+//   const acceptVal = elements.accept.checked;
+//   console.log(loginVal, pass1Val, pass2Val, acceptVal);
+//   const errors = [];
+//   if (!loginVal.includes('@')) {
+//     errors.push('[for="formLogin"]');
+//   }
+//   if (pass1Val !== pass2Val || pass1Val.length < 6) {
+//     errors.push('[for="formPass1"]', '[for="formPass2"]');
+//   }
+//   if (!acceptVal) {
+//     errors.push('[for="formAccept"]');
+//   }
+//   const labelsList = document.querySelectorAll('label');
+//   labelsList.forEach((label) => {
+//     label.style.color = '';
+//   });
+
+//   if (errors.length === 0) {
+//     console.log('done');
+//   } else {
+//     console.log(errors);
+//     errors.forEach((selector) => {
+//       const el = document.querySelector(selector);
+//       console.log(el);
+//       el.style.color = 'red';
+//     });
+//   }
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
+  console.log(form);
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const elements = e.target.elements;
+    const loginVal = elements.login.value;
+    const pass1Val = elements.pass1.value;
+    const pass2Val = elements.pass2.value;
+    const acceptVal = elements.accept.checked;
+    console.log(loginVal, pass1Val, pass2Val, acceptVal);
+    const errors = [];
+    if (!loginVal.includes('@')) {
+      errors.push('[for="formLogin"]');
+    }
+    if (pass1Val !== pass2Val || pass1Val.length < 6) {
+      errors.push('[for="formPass1"]', '[for="formPass2"]');
+    }
+    if (!acceptVal) {
+      errors.push('[for="formAccept"]');
+    }
+    const labelsList = document.querySelectorAll('label');
+    labelsList.forEach((label) => {
+      label.style.color = '';
+    });
+
+    if (errors.length === 0) {
+      console.log('done');
+    } else {
+      errors.forEach((selector) => {
+        const el = document.querySelector(selector);
+        el.style.color = 'red';
+      });
+    }
+  });
+});
