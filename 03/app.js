1c9294c0ef2973773583cb73fbd4c70e089a6a0d
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2021-02-25 13:21:12 +0100

    Zadanie nr 3 zrobione

---
+++ 03/app.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  function roundNumber(number) {
+    return number.toFixed(2);
+  }
+  const inputEl = document.querySelector('input');
+  const imagesList = document.querySelector('.images-list');
+  const imageItemProto = imagesList.children[0];
+  inputEl.addEventListener('change', e => {
+
+    const filesList = e.target.files;
+    const filesListArr = [...filesList];
+    filesListArr.forEach(file => {
+      const reader = new FileReader();
+      reader.onload = (readerEvent) => {
+        const liEl = imageItemProto.cloneNode(true);
+        liEl.classList.remove('images-list__item--prototype');
+        [header, img, footer] = liEl.children;
+        header.innerText = file.name;
+        img.src = readerEvent.target.result;
+        const fileSizeInMb = file.size / 1048576;
+
+        footer.innerText = `${roundNumber(fileSizeInMb)}MB`;
+        imagesList.appendChild(liEl);
+      }
+      reader.readAsDataURL(file);
+    })
+  })
+})
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const inputEl = document.querySelector('input');
+//   const imagesList = document.querySelector('.images-list');
+//   const imageItemProto = imagesList.children[0];
+//   inputEl.addEventListener('change', (e) => {
+//     const filesList = e.target.files;
+//     const filesArr = [...filesList];
+//     filesArr.forEach((file) => {
+//       const reader = new FileReader();
+//       reader.onload = (file, readerEvent) => {
+//         const liEl = imageItemProto.cloneNode(true);
+//         liEl.classList.remove('images-list__item--prototype');
+//         const [header, img, footer] = liEl.children;
+//         img.src = readerEvent.target.result;
+//         header.innerText = `${file.name}`;
+//       }
+//       reader.readAsDataURL(file);
+//     });
+//   });
+// });
