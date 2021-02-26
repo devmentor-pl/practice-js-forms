66190cdf00bea10b32af035452db416be3c25e05
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2021-02-26 16:43:50 +0100

    Dodane if/else zeby pozwalac na zaladowanie tylko plikow graficznych

--- 03/app.js
+++ 03/app.js
@@ -7,23 +7,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const imagesList = document.querySelector('.images-list');
   const imageItemProto = imagesList.children[0];
   inputEl.addEventListener('change', e => {
-
     const filesList = e.target.files;
     const filesListArr = [...filesList];
     filesListArr.forEach(file => {
-      const reader = new FileReader();
-      reader.onload = (readerEvent) => {
-        const liEl = imageItemProto.cloneNode(true);
-        liEl.classList.remove('images-list__item--prototype');
-        [header, img, footer] = liEl.children;
-        header.innerText = file.name;
-        img.src = readerEvent.target.result;
-        const fileSizeInMb = file.size / 1048576;
-
-        footer.innerText = `${roundNumber(fileSizeInMb)}MB`;
-        imagesList.appendChild(liEl);
+      if(file && file.type.includes('image')) {
+        const reader = new FileReader();
+        reader.onload = (readerEvent) => {
+          const liEl = imageItemProto.cloneNode(true);
+          liEl.classList.remove('images-list__item--prototype');
+          [header, img, footer] = liEl.children;
+          header.innerText = file.name;
+          img.src = readerEvent.target.result;
+          const fileSizeInMb = file.size / 1048576;
+  
+          footer.innerText = `${roundNumber(fileSizeInMb)}MB`;
+          imagesList.appendChild(liEl);
+        }
+        reader.readAsDataURL(file);
+      } else {
+        alert('nie wybrano pliku graficznego')
       }
-      reader.readAsDataURL(file);
-    })
   })
-})
+})})
