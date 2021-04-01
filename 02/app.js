594116f3c68507e6fed5bb545fb52adc899de3e4
Agnieszka Polińska <agapoli@gmail.com>
2021-04-01 07:04:14 +0200

    Handle errors

--- 02/app.js
+++ 02/app.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", (e) => {
     submitForm();
 });
 
-const errors = [];
+let errors = [];
+let correctData = []; //dodatkowa zmienna stworzona by obsłużyć zmianę koloru z czerwonego na czarny, gdy dane zostały poprawione
 
 const submitForm = () => {
     const form = document.querySelector('form');
@@ -10,27 +11,31 @@ const submitForm = () => {
 }
 
 const validate = (e) => {
+    errors = [];
+    correctData = [];
     e.preventDefault();
 
     const loginInput = document.querySelector('#formLogin');
     const pass1Input = document.querySelector('#formPass1');
     const pass2Input = document.querySelector('#formPass2');
     const formAccept = document.querySelector('#formAccept');
-    console.log(formAccept.checked);
-    //validateLogin(loginInput);
-    //validatePasswords(pass1Input, pass2Input);
-    //validateAccept(formAccept);
 
-    console.log(errors)
+    const validatedLogin = validateLogin(loginInput);
+    const validatedPasswords = validatePasswords(pass1Input, pass2Input);
+    const validatedAccept = validateAccept(formAccept);
 
-    if (validateLogin(loginInput) && validatePasswords(pass1Input, pass2Input) && validateAccept(formAccept)) {
-        console.log("done")
+    if ( validatedLogin &&  validatedPasswords && validatedAccept) {
+        console.log("done");
+        correctData.forEach(correct => {
+            console.log(correct);
+            correct.previousElementSibling.style.color = "black";
+        })
     } else {
         errors.forEach(error => {
-            console.log(error.id)
+            console.log(error);
+            error.previousElementSibling.style.color = "red";
         })
-    }
-    
+    } 
 }
 
 const validateLogin = loginInput => {
@@ -41,6 +46,7 @@ const validateLogin = loginInput => {
     if (!re.test(String(login).toLowerCase())) {
         errors.push(loginInput);
     } else {
+        correctData.push(loginInput);
         return true;
     }
 }
@@ -50,17 +56,20 @@ const validatePasswords = (pass1Input, pass2Input) => {
     const pass2 = pass2Input.value;
 
     if (pass1.length < 6 || pass1 !== pass2) {
-        errors.push(pass1Input)
-        errors.push(pass2Input)
+        errors.push(pass1Input);
+        errors.push(pass2Input);
     } else {
+        correctData.push(pass1Input);
+        correctData.push(pass2Input);
         return true;
     }
 }
 
 const validateAccept = formAccept => {
     if (!formAccept.checked) {
-        errors.push(formAccept)
+        errors.push(formAccept);
     } else {
+        correctData.push(formAccept);
         return true;
     }
 }
