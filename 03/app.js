9b65d79d6f57ba2598a263d2655895ac16ab3277
Agnieszka Polińska <agapoli@gmail.com>
2021-04-06 17:52:35 +0200

    Use FileReader to get file from user

--- 03/app.js
+++ 03/app.js
@@ -8,15 +8,20 @@ input.addEventListener('change', (e) => {
     const allFiles = Array.from(e.target.files);
 
     allFiles.forEach(file => {
-        const li = liToClone.cloneNode(true);
-        li.classList.remove('images-list__item--prototype');
-        const ul = liToClone.parentElement;
-        ul.appendChild(li);
+        const fileReader = new FileReader();
+        fileReader.onload = (e) => {
+            const li = liToClone.cloneNode(true);
+            li.classList.remove('images-list__item--prototype');
+            const ul = liToClone.parentElement;
+            ul.appendChild(li);
 
-        li.querySelector(".images-list__item-name").textContent = file.name;
-        li.querySelector(".images-list__item-img").src = file.name;
+            li.querySelector(".images-list__item-name").textContent = file.name;
+            li.querySelector(".images-list__item-img").src = e.target.result;
 
-        const mbSize = (file.size / (1024*1024)).toFixed(2);
-        li.querySelector(".images-list__item-size").textContent = `rozmiar: ${mbSize} mb`;
+            const mbSize = (file.size / (1024*1024)).toFixed(2);
+            li.querySelector(".images-list__item-size").textContent = `rozmiar: ${mbSize} MB`;
+        }
+
+        fileReader.readAsDataURL(file)
     }); 
 })
