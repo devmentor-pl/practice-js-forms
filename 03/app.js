b196e7f1bb99b5324a97c468c2338c759de934c1
Malgorzata Kowacka <kowackag@gmail.com>
2021-07-04 13:12:09 +0200

    Change task 03
    Add function prepareImage(file)

--- 03/app.js
+++ 03/app.js
@@ -8,21 +8,25 @@ function displayImage(e) {
     for (let i = 0; i < numFiles; i++) {
         const file = e.target.files[i];
         if (file && file.type.includes('image')) {
-            const newLi = liEl.cloneNode(true);
-            newLi.classList.remove('images-list__item--prototype');
-            ulEl.appendChild(newLi);
-            const size = file.size;
-            const name = file.name;
-            const header = newLi.querySelector('header');
-            const footer = newLi.querySelector('footer');
-            header.innerText = name;
-            footer.innerText = size;
-            const reader = new FileReader();
-            reader.onload = function (event) {
-                const img = newLi.querySelector('img')
-                img.src = event.target.result;
-            }
-            reader.readAsDataURL(file);
+            prepareImage(file);
         }
     }
+}
+
+function prepareImage(file) {
+    const newLi = liEl.cloneNode(true);
+    newLi.classList.remove('images-list__item--prototype');
+    ulEl.appendChild(newLi);
+    const size = file.size;
+    const name = file.name;
+    const header = newLi.querySelector('header');
+    const footer = newLi.querySelector('footer');
+    header.innerText = name;
+    footer.innerText = size;
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const img = newLi.querySelector('img')
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(file);
 }
