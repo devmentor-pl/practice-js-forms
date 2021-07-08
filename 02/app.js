a92c2d816aab8cc7d843e6ec0e4ae9658b549270
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-07-08 11:08:04 +0100

    add color changed if input is correct

--- 02/app.js
+++ 02/app.js
@@ -4,29 +4,35 @@ const checkEmail = function (e, arr) {
   const email = e.target.elements.login.value;
   const emailFormat =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const emailInput = e.target.elements.login;
+  const label = emailInput.parentNode.querySelector("label");
   if (!email.match(emailFormat)) {
-    const emailInput = e.target.elements.login;
-    const label = emailInput.parentNode.querySelector("label");
     arr.push(label);
     console.log(arr);
+  } else {
+    label.style.color = "black";
   }
 };
 
 const checkPassword = function (e, pass1, arr) {
+  const pass1Input = e.target.elements.pass1;
+  const label = pass1Input.parentNode.querySelector("label");
   if (pass1.length < 6) {
-    const pass1Input = e.target.elements.pass1;
-    const label = pass1Input.parentNode.querySelector("label");
     arr.push(label);
     console.log(arr);
+  } else {
+    label.style.color = "black";
   }
 };
 
 const checkPassword2 = function (e, pass2, arr) {
+  const pass2Input = e.target.elements.pass2;
+  const label = pass2Input.parentNode.querySelector("label");
   if (pass2.length < 6) {
-    const pass2Input = e.target.elements.pass2;
-    const label = pass2Input.parentNode.querySelector("label");
     arr.push(label);
     console.log(arr);
+  } else {
+    label.style.color = "black";
   }
 };
 
@@ -35,20 +41,24 @@ const checkIfPasswordsMatch = function (e, arr) {
   checkPassword(e, pass1, arr);
   const pass2 = e.target.elements.pass2.value;
   checkPassword2(e, pass2, arr);
+  const pass2Input = e.target.elements.pass2;
+  const label = pass2Input.parentNode.querySelector("label");
   if (pass1 !== pass2) {
-    const pass2Input = e.target.elements.pass2;
-    const label = pass2Input.parentNode.querySelector("label");
     arr.push(label);
     console.log(arr);
+  } else {
+    label.style.color = "black";
   }
 };
 
 const isRegChecked = function (e, arr) {
   const accept = e.target.elements["accept"];
+  const label = accept.parentNode.querySelector("label");
   if (!accept.checked) {
-    const label = accept.parentNode.querySelector("label");
     arr.push(label);
     console.log(arr);
+  } else {
+    label.style.color = "black";
   }
 };
 
