8f7a43241a0f42135641be8e2c3759e18dfd14c6
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-07-09 13:43:47 +0100

    last fix 01

--- 01/app.js
+++ 01/app.js
@@ -1,32 +1,11 @@
-// 1) stara wersja
-/*
-const form = document.querySelector("form");
-const list = document.querySelector(".users-list");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const name = e.target.elements.firstName.value;
-  const surname = e.target.elements.lastName.value;
-
-  const newPerson = document.createElement("li");
-  newPerson.innerText = name + " " + surname;
-  list.appendChild(newPerson);
-});
-*/
-
-// 2)
+const init = function () {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", handleSubmit);
+};
 
 document.addEventListener("DOMContentLoaded", init);
 
-function init() {
-  const form = document.querySelector("form");
-}
-
-const form = document.querySelector("form");
-
-// nie moge bez ponownego zadeklarowania "form" miec dostepu do tej zmiennej, nie moge jej zadeklarowac wewnatrz eventu poniewaz to na niej potrzebuje uzyc event listenera. Zwrocenie jej funckji tez nie zadziala poniewaz nie uzywam jej jako parametru.
-form.addEventListener("submit", function (e) {
+const handleSubmit = function (e) {
   e.preventDefault();
 
   const name = e.target.elements.firstName.value;
@@ -36,4 +15,4 @@ form.addEventListener("submit", function (e) {
   newPerson.innerText = name + " " + surname;
   const list = document.querySelector(".users-list");
   list.appendChild(newPerson);
-});
+};
