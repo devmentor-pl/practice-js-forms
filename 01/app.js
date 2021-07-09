eae0314f6c6677970acfd79e227eaecfae6eb2ce
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-07-09 10:49:30 +0100

    add solution with DOMContentLoad

--- 01/app.js
+++ 01/app.js
@@ -1,3 +1,5 @@
+// 1) stara wersja
+/*
 const form = document.querySelector("form");
 const list = document.querySelector(".users-list");
 
@@ -9,5 +11,29 @@ form.addEventListener("submit", function (e) {
 
   const newPerson = document.createElement("li");
   newPerson.innerText = name + " " + surname;
+  list.appendChild(newPerson);
+});
+*/
+
+// 2)
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const form = document.querySelector("form");
+}
+
+const form = document.querySelector("form");
+
+// nie moge bez ponownego zadeklarowania "form" miec dostepu do tej zmiennej, nie moge jej zadeklarowac wewnatrz eventu poniewaz to na niej potrzebuje uzyc event listenera. Zwrocenie jej funckji tez nie zadziala poniewaz nie uzywam jej jako parametru.
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = e.target.elements.firstName.value;
+  const surname = e.target.elements.lastName.value;
+
+  const newPerson = document.createElement("li");
+  newPerson.innerText = name + " " + surname;
+  const list = document.querySelector(".users-list");
   list.appendChild(newPerson);
 });
