c7009b2354cfba5a184a134bdb5307835dc03585
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-07-23 20:14:39 +0200

    optimized code, use nodeList

--- 01/app.js
+++ 01/app.js
@@ -1,6 +1,5 @@
 const formElement = document.querySelector('form');
-const inputElementFirstName = document.querySelector('[name=firstName]')
-const inputElementLastName = document.querySelector('[name=lastName]')
+const inputElements = formElement.querySelectorAll('input[name]')
 const ulElement = document.querySelector('.users-list');
 
 function getUserInfo (e) {
@@ -9,8 +8,9 @@ function getUserInfo (e) {
   let lastName = e.target.elements.lastName.value
 
   if(!(firstName.length === 0) && !(lastName.length === 0)) {
-    inputElementFirstName.style.border = '2px solid green';
-    inputElementLastName.style.border = '2px solid green';
+    inputElements.forEach(item => {
+      item.style.border = '2px solid green';
+    })
     let liElement = document.createElement('li');
     liElement.classList.add('users-list__person');
     liElement.textContent = `${firstName} ${lastName}`
