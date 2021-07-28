0f49f3b3ad9879005e659189122b4dc81d748431
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-07-28 15:25:23 +0200

    add checkdata function

---
+++ 05/app.js
@@ -0,0 +1,50 @@
+const formElement = document.querySelector('form');
+formElement.noValidate = true;
+formElement.autocomplete = 'off';
+const ulElement = formElement.querySelector('.messages');
+const validationObj = {
+  firstName : formElement.elements.firstName,
+  lastName : formElement.elements.lastName,
+  streetName : formElement.elements.street,
+  houseNumber : formElement.elements.houseNumber,
+  zipCode : formElement.elements.zip,
+  city : formElement.elements.city,
+  voivodeship : formElement.elements.voivodeship
+}
+
+let errors = [];
+
+formElement.addEventListener('submit', checkData)
+
+function checkLength (obj) {
+  for(let el in obj) {
+    if(obj[el].value < 1) {
+      errors.push(obj[el])
+    }
+  }
+}
+
+function checkData (e) {
+  checkLength(validationObj);
+  if(errors.length > 0) {
+    e.preventDefault();
+    errors.forEach(item => {
+      item.style.border = '1px solid red';
+      let liElement = document.createElement('li');
+      liElement.textContent = `Uzupełnij pole ${item.parentElement.innerText}`
+      ulElement.appendChild(liElement);
+    })
+    errors = [];
+  } else {
+    e.preventDefault()
+    let liElementCancel = ulElement.querySelectorAll('li');
+    liElementCancel.forEach(item => {
+      ulElement.removeChild(item);
+    })
+    alert('done');
+  }
+}
+
+
+
+
