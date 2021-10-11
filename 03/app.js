d326117a022bcf0f0d1bf0863116b9a1168c837c
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-10-11 23:55:28 +0200

    Third task

---
+++ 03/app.js
@@ -0,0 +1,36 @@
+const inputEl = document.querySelector('input');
+// const imageList = document.querySelector('.images-list');
+const liElement = document.querySelector('.images-list__item--prototype');
+console.log(liElement);
+console.log(inputEl);
+// console.log(imageList);
+
+inputEl.addEventListener('change', function(el){
+    const file = [...el.target.files];
+
+    if (file) {
+        file.forEach(function(el){
+
+            if (el.type.includes('image')) {
+
+                const clonedPrototype = liElement.cloneNode(true);
+                liElement.parentElement.appendChild(clonedPrototype);
+
+                const reader = new FileReader();
+                reader.onload = function(readerEvent){
+                    clonedPrototype.querySelector('img').src = readerEvent.target.result;
+                }
+                reader.readAsDataURL(el);
+
+                let imgSizeKB = el.size / 1024;
+                let imgSizeMB = imgSizeKB.toFixed(2);
+
+                clonedPrototype.querySelector('header').innerText = el.name;
+                clonedPrototype.querySelector('footer').innerText = imgSizeMB + " MB";
+
+
+                clonedPrototype.style.display = 'block';
+            }
+        });
+    }
+});
