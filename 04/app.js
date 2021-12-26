9d61628ecaf1126db1dae0b45f37e68577daff80
Ewelina <ewelina@ekopacz.pl>
2021-12-26 11:58:49 +0100

    added solution

--- 04/app.js
+++ 04/app.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', init);
+const boxElement = document.querySelector('.box');
+const pannelEl = document.querySelector('.panel');
+// const inputColor = document.querySelector('input[name="color"]');
+// const inputRange = document.querySelector('input[name="opacity"]');
 
 function init() {
-    const boxElement = document.querySelector('.box');
+    // const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
 }
 
-function setBoxShadow(element, colorInHex, opacity = 1) {
+function setBoxShadow(element, colorInHex, opacity=1) {
     const colorInRGBA = `rgba(
-        ${getChannelColor(colorInHex, 'red')}, 
-        ${getChannelColor(colorInHex, 'green')}, 
-        ${getChannelColor(colorInHex, 'blue')}, 
+        ${getChannelColor(colorInHex, 'red')},
+        ${getChannelColor(colorInHex, 'green')},
+        ${getChannelColor(colorInHex, 'blue')},
         ${opacity}
     )`;
-
+    console.log(opacity);
     element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
 }
-
 
 function getChannelColor(colorInHex, channelName) {
     let start;
@@ -31,10 +34,37 @@ function getChannelColor(colorInHex, channelName) {
             break;
     }
 
-    const channelColorHex = colorInHex.substr(start, 2);
+    const channelColorHex = colorInHex.substr(start, 2); // substr(start,lenght) - Zwraca określoną liczbę początkowych znaków w łańcuchu znaków w określonej lokalizacji.
     const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+    return channelColorDec;
 }
 
+pannelEl.addEventListener('change', function(e){
+    let pickedRange;
+    if(e.target.type ==='color'){
+        pickedColor = e.target.value;
+    }
+    if(e.target.type ==='range'){
+        pickedRange = Number(e.target.value)/100;
+    }
+    setBoxShadow(boxElement,pickedColor,pickedRange);
+});
 
+// inputColor.addEventListener('change', function(e){
+//     pickedColor = e.target.value;
+//     setBoxShadow(boxElement,pickedColor);
+// });
+
+// inputRange.addEventListener('mouseover',function(e){
+//     if(e.target.type ==='range'){
+//         range = e.target.value;
+//         console.log(range);
+//     }
+// });
+// inputRange.addEventListener('change',function(e){
+//     if(e.target.type ==='range'){
+//         range = e.target.value;
+//         console.log(range);
+//     }
+// });
