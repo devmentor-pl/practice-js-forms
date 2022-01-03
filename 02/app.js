13ac72ad7604fc3a5b48a278a59bdfac3e1f24fa
Ewelina <ewelina@ekopacz.pl>
2022-01-03 13:02:29 +0100

    add missing method

--- 02/app.js
+++ 02/app.js
@@ -1,17 +1,15 @@
 const formEl = document.forms[0];
 formEl.addEventListener('submit', checkData);
+const labelEls = document.querySelectorAll('label');
 
 function checkData(e){
     e.preventDefault();
+
     const errorValues = [];
-    const correctValues=[];
 
     const emailValue = e.target.elements.login.value;
     if(!checkEmail(emailValue)){
         errorValues.push(e.target.elements.login);
-    }
-    else {
-        correctValues.push(e.target.elements.login);
     }
 
     const pass1Value = e.target.elements.pass1.value;
@@ -20,21 +18,12 @@ function checkData(e){
         errorValues.push(e.target.elements.pass1);
         errorValues.push(e.target.elements.pass2);
     }
-    else{
-        correctValues.push(e.target.elements.pass1);
-        correctValues.push(e.target.elements.pass2);
-    }
 
     const acceptBoxValue = e.target.elements.accept;
     if(!acceptBoxValue.checked){
         errorValues.push(acceptBoxValue);
     }
-    else {
-        correctValues.push(acceptBoxValue);
-    }
-
     showIncorrectValues(errorValues);
-    showCorrectValues(correctValues);
 }
 
 function checkEmail(dataEmail){
@@ -55,20 +44,20 @@ function checkPassword(pass1Value, pass2Value){
 }
 
 function showIncorrectValues(errorValues){
-    if(!errorValues.length){
-        console.log("Done")
+    labelEls.forEach(function(item){
+        item.style.color = 'black';
+    })
+
+    if(errorValues.length>0){
+    errorValues.forEach(function(item){
+        item.previousElementSibling.style.color= "red";
+
+    });
     }
     else {
-        // alert('Formularz zawiera bledy - wpisz poprawne dane')
-        errorValues.forEach(function(item){
-            item.previousElementSibling.style.color= "red";
-        });
+        console.log("Done");
     }
 }
 
-function showCorrectValues(correctValues){
-    correctValues.forEach(function(item){
-        item.previousElementSibling.style.color= "green";
-    });
-}
+
 
