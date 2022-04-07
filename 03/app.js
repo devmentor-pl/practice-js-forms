abc02b4f7bd53209a6fe800a4d3bb99201e4ea24
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-04-07 13:05:59 +0200

    Add changes to exercise 03 and solution to exercise 04

---
+++ 03/app.js
@@ -0,0 +1,2 @@
+const fileEl = document.querySelector('file');
+
--- 04/app.js
+++ 04/app.js
@@ -4,6 +4,46 @@ function init() {
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
 }
+//const colorEl = document.querySelector('[type = "color"]');
+//const rangeEl = document.querySelector('[type = "range"]');
+const boxElement = document.querySelector('.box');
+
+function changeShadow(e){
+    const colorEl = document.querySelector('[type = "color"]');
+    const rangeEl = document.querySelector('[type = "range"]');
+    const color = colorEl.value;
+    const range = 1 - (rangeEl.value/100);
+    console.log(color, range);
+    setBoxShadow(boxElement, color, range);
+    colorEl.addEventListener('change', changeShadow);
+   rangeEl.addEventListener('change', changeShadow);
+}
+changeShadow(boxElement);
+//colorEl.addEventListener('change', changeShadow);
+//rangeEl.addEventListener('change', changeShadow);
+
+// drugie rozwiązanie, pierwsze podoba mi się bardziej, ale zostawiam oba
+
+/*let newColorInHex = '#000000';
+let newOpacity = 1;
+
+function changeShadowColor(e) {
+    newColorInHex = e.target.value;
+    const boxElement = document.querySelector('.box');
+    setBoxShadow(boxElement, newColorInHex, newOpacity);
+}
+
+function changeShadowOpacity(e) {
+    newOpacity = 1 - (e.target.value/100);
+    const boxElement = document.querySelector('.box');
+    setBoxShadow(boxElement, newColorInHex, newOpacity);
+}
+
+const colorEl = document.querySelector('[type = "color"]');
+const rangeEl = document.querySelector('[type = "range"]');
+
+colorEl.addEventListener('change', changeShadowColor);
+rangeEl.addEventListener('change', changeShadowOpacity);*/
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
     const colorInRGBA = `rgba(
@@ -36,5 +76,7 @@ function getChannelColor(colorInHex, channelName) {
 
     return channelColorDec; 
 }
+
+
 
 
