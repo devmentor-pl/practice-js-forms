59be032597c5ce7e05d12134ae3cd104cbe0b119
Andrzej <andis_as@wp.pl>
2022-04-25 20:17:57 +0200

    zad 2 - solution Two: error in array

--- 02/app.js
+++ 02/app.js
@@ -1,4 +1,76 @@
 
+// solution One first error - set label red
+// -----------------------------------
+// const form = document.querySelector('form')
+// form.noValidate = true
+// form.autocomplete = 'off'
+// console.log(form.noValidate)
+// console.log(form.autocomplete)
+
+// const handleSubmit = function(e) {
+//     e.preventDefault()
+//     console.log('submit')
+
+//     const label = document.querySelectorAll('label')
+//     label.forEach(lab => {
+//         lab.removeAttribute('style')
+//     })
+
+//     // email
+//     const email = form.login
+//     console.log('email:', email.value)
+
+//     if(!email.value.includes('@')) {
+//         console.log('email - brak @')
+//         const errorEmail = email.previousElementSibling
+//         errorEmail.style.color = 'red'
+//         return
+//     } 
+
+//     // password
+//     const pass1 = form.pass1
+//     const pass2 = form.pass2
+
+//     if(pass1.value.length < 6) {
+//         console.log('password 1 must have over 6 chars')
+//         const errorPass1 = pass1.previousElementSibling
+//         errorPass1.style.color = 'red'
+//         return
+//     }
+//     if(pass2.value.length < 6) {
+//         console.log('password 2 must have over 6 chars')
+//         const errorPass2 = pass2.previousElementSibling
+//         errorPass2.style.color = 'red'
+//         return
+//     }
+//     if(pass1.value !== pass2.value) {
+//         console.log('passwords are different')
+//         const errorPass1 = pass1.previousElementSibling
+//         const errorPass2 = pass2.previousElementSibling
+//         errorPass1.style.color = 'red'
+//         errorPass2.style.color = 'red'
+//         return
+//     }
+
+//     // regulation checkbox
+//     const reg = form.accept 
+//     if(reg.checked) {
+//         console.log('Regulation checked :)')
+//     } else {
+//         console.log('Regulation not checked')
+//         const errorReg = reg.previousElementSibling
+//         errorReg.style.color = 'red'
+//         return
+//     }
+
+//     // send data
+//     console.log('Data send ... DONE')
+// }
+// form.addEventListener('submit', handleSubmit)
+
+
+// solution Two errors in Array
+// -----------------------------------
 const form = document.querySelector('form')
 form.noValidate = true
 form.autocomplete = 'off'
@@ -8,6 +80,7 @@ console.log(form.autocomplete)
 const handleSubmit = function(e) {
     e.preventDefault()
     console.log('submit')
+    const errors = []
 
     const label = document.querySelectorAll('label')
     label.forEach(lab => {
@@ -20,9 +93,8 @@ const handleSubmit = function(e) {
 
     if(!email.value.includes('@')) {
         console.log('email - brak @')
-        const errorEmail = email.previousElementSibling
-        errorEmail.style.color = 'red'
-        return
+        errors.push(email)
+        console.log(errors)
     } 
 
     // password
@@ -31,23 +103,21 @@ const handleSubmit = function(e) {
 
     if(pass1.value.length < 6) {
         console.log('password 1 must have over 6 chars')
-        const errorPass1 = pass1.previousElementSibling
-        errorPass1.style.color = 'red'
-        return
+        errors.push(pass1)
+        console.log(errors)
     }
     if(pass2.value.length < 6) {
         console.log('password 2 must have over 6 chars')
-        const errorPass2 = pass2.previousElementSibling
-        errorPass2.style.color = 'red'
-        return
+        errors.push(pass2)
+        console.log(errors)
     }
-    if(pass1.value !== pass2.value) {
-        console.log('passwords are different')
-        const errorPass1 = pass1.previousElementSibling
-        const errorPass2 = pass2.previousElementSibling
-        errorPass1.style.color = 'red'
-        errorPass2.style.color = 'red'
-        return
+    if(pass1.value.length >= 6 && pass2.value.length >= 6) {
+        if(pass1.value !== pass2.value) {
+            console.log('passwords are different')
+            errors.push(pass1)
+            errors.push(pass2)
+            console.log(errors)
+        }
     }
 
     // regulation checkbox
@@ -56,13 +126,20 @@ const handleSubmit = function(e) {
         console.log('Regulation checked :)')
     } else {
         console.log('Regulation not checked')
-        const errorReg = reg.previousElementSibling
-        errorReg.style.color = 'red'
-        return
+        errors.push(reg)
+        console.log(errors)
     }
 
+    errors.forEach(item => {
+        const elementError = item.previousElementSibling
+        elementError.style.color = 'red'
+    })
     // send data
-    console.log('Data send ... DONE')
+    if(errors.length === 0) {
+        console.log('Data send ... DONE')
+    } else {
+        console.log('Form not completed. so NOT sent')
+    }
 }
 
 form.addEventListener('submit', handleSubmit)
