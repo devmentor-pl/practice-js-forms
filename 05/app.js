fbc836d71f02b1a156b1f48c22a37211b173352e
Andrzej <andis_as@wp.pl>
2022-04-28 16:59:58 +0200

    zad 5 - validation made

--- 05/app.js
+++ 05/app.js
@@ -11,30 +11,40 @@ function checkForm(e) {
     const firstName = form.elements.firstName.value
     if(firstName.length === 0) {
         errors.push('Imie - pole nie może być puste')
+    } else if(!isNaN(firstName)) {
+        errors.push('Imię - pole musi być tekstowe')
     }
 
     // lastName
     const lastName = form.elements.lastName.value
     if(lastName.length === 0) {
         errors.push('Nazwisko - pole nie może być puste')
+    } else if(!isNaN(lastName)) {
+        errors.push('Nazwisko - pole musi być tekstowe')
     }
 
     // street
     const street = form.elements.street.value
     if(street.length === 0) {
         errors.push('Ulica - pole nie może być puste')
+    } else if(!isNaN(street)) {
+        errors.push('Ulica - pole musi być tekstowe')
     }
 
     // houseNumber
     const houseNumber = form.elements.houseNumber.value
     if(houseNumber.length === 0) {
         errors.push('Numer budynku - pole nie może być puste')
+    } else if (houseNumber <= 0) {
+        errors.push('Numer budynku - podaj poprawną wartość')
     }
 
     // flatNumber
     const flatNumber = form.elements.flatNumber.value
     if(flatNumber.length === 0) {
         errors.push('Numer mieszkania - pole nie może być puste')
+    } else if (flatNumber <= 0) {
+        errors.push('Numer mieszkania - podaj poprawną wartość')
     }
 
     // Kod pocztowy
@@ -69,6 +79,9 @@ function checkForm(e) {
     messages.innerText = ''
     if(errors.length === 0) {
         console.log('Form send')
+        setTimeout(function() {
+            window.alert('FORM has been send')
+        })
     } else {
         errors.forEach(error => {
             const li = document.createElement('li')
