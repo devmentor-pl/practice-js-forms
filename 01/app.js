657b8aa1be850dbead8c1a99b57f3d5186722b48
Lukasz Scisel <lukscidev@gmail.com>
2022-05-10 20:04:14 +0200

    01 done

---
+++ 01/app.js
@@ -0,0 +1,15 @@
+const formEl = document.querySelector('form');
+
+formEl.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const firstName = formEl.elements[0].value;
+    const lastName = formEl.elements['lastName'].value;
+    const list = document.querySelector('.users-list');
+
+    if (firstName.length > 0 && lastName.length > 0) {
+        const liEl = document.createElement('li')
+        liEl.classList.add('user-list__person');
+        liEl.innerText = `${firstName} ${lastName}`
+        list.appendChild(liEl);
+    }
+})
