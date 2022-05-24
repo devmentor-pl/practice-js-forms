63ddb24818bd2cd0cc43f007a35cdc65ff2f0368
justyna <strawberka17@gmail.com>
2022-05-24 18:57:28 +0100

    fixed app5

--- 05/app.js
+++ 05/app.js
@@ -8,16 +8,33 @@ function init(){
     const ul=document.querySelector('.messages')
     
 
+
     function checkData(e){
         e.preventDefault()
-    const firstName=form.elements.firstName
+
+     const  fields=[
+            {name:'firstName',required:true},
+            {name:'lastName',required: true},
+            {name:'street',required: true},
+            {name:'houseNumber',type:'number',required:true},
+            {name:'flatNumber',type:'number',required  :true},
+            {name:'zip',pattern:/[0-9]{2}-[0-9]{3}/,required:true},
+             {name:'city',required:true},
+             {name:'voivodeship',required:true},
+
+        ]
+    
+    
+    
+    
+        const firstName=form.elements.firstName
     const lastName=form.elements.lastName
     const street=form.elements.street;
     const houseNumber=form.elements.name.houseNumber
-    const flatName=form.elements.flatNumber
+    const flatNumber=form.elements.flatNumber
     const zip=form.elements.zip
     const city=form.elements.city
-    const fvoivodeship=form.elements.voivodeship;
+    const voivodeship=form.elements.voivodeship;
     const errors=[]
     
     
@@ -31,6 +48,9 @@ function init(){
         if(street.value.length ===0){
         errors.push('please write correct name')
     }
+     if(flatName.value.length ===0){
+        errors.push('please write correct name')
+    }
       if(Number.isNaN(Number(houseNumber.value))){
         errors.push('please write correct name')
     }
@@ -41,7 +61,7 @@ function init(){
     if(!/[0-9]{2}-[0-9]{3}/.test(zip.value)){
  errors.push('please write correct pocztowy')
     }
- if(fcity.value.length ===0){
+ if(city.value.length ===0){
         errors.push('please write correct name')}
  if(voivodeship.value.length ===0){
         errors.push('please write correct name')}
