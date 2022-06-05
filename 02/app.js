28b29051ce7c28aa84d15af9206a90a0b5419076
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-06-05 18:36:38 +0200

    fix 02

--- 02/app.js
+++ 02/app.js
@@ -1,62 +1,58 @@
-
 const formEl = document.querySelector('form');
 
-
 function registerUser(e) {
-    e.preventDefault();
+	e.preventDefault();
 	const emailEl = e.target.elements.login;
 	const firstPass = e.target.elements.pass1;
 	const secondPass = e.target.elements.pass2;
 	const wrongPass = false;
 	const accept = e.target.elements.accept;
-    
+
 	const errors = [];
-	
+
 	if (!emailEl.value.includes('@')) {
-        errors.push(emailEl);
+		errors.push(emailEl);
 	}
-    
+
 	if (firstPass.value.length < 6) {
-        errors.push(firstPass);
+		errors.push(firstPass);
 	}
-    
+
 	if (secondPass.value.length < 6) {
-        errors.push(secondPass);
+		errors.push(secondPass);
 	}
-    
+
 	if (firstPass.value !== secondPass.value) {
-        errors.push(wrongPass);
+		errors.push(wrongPass);
 	}
-    
+
 	if (!accept.checked) {
-        errors.push(accept);
+		errors.push(accept);
 	}
-    
+
 	for (const el of formEl.elements) {
-        if (el.previousElementSibling) {
-            errorsView(el, 'black');
+		if (el.previousElementSibling) {
+			errorsView(el, 'black');
 		}
 	}
-    
+
 	if (errors.length > 0) {
-        e.preventDefault();
-        
+		e.preventDefault();
+
 		errors.forEach(function (element) {
-            if (element) {
-                errorsView(element, 'red');
-			}
-            
-			if (!element) {
-                pass2El.previousElementSibling.style.color = 'red';
+			if (element) {
+				errorsView(element, 'red');
+			} else {
+				pass2El.previousElementSibling.style.color = 'red';
 			}
 		});
 	}
 }
 
 const errorsView = function (el, color) {
-    const previouseEl = el.previousElementSibling;
+	const previouseEl = el.previousElementSibling;
 	if (previouseEl !== undefined) {
-        el.previousElementSibling.style.color = color;
+		el.previousElementSibling.style.color = color;
 	}
 };
 
