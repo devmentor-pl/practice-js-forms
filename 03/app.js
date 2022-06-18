abbadb9aa450bac14c7a45ced70b3b404a55b0f4
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-06-18 22:24:35 +0200

    #03 done

--- 03/app.js
+++ 03/app.js
@@ -1,32 +1,45 @@
 const fileEl = document.querySelector('input')
-const liElements = document.querySelector('.images-list__item--prototype')
-const imgElementName = document.querySelector('.images-list__item-name')
-const imgElement = document.querySelector('.images-list__item-img')
-const imgElementSize = document.querySelector('.images-list__item-size')
+const liElement = document.querySelector('.images-list__item--prototype')
 
 fileEl.addEventListener('change', readFile)
 
-function readFile(e){
-    const file = e.target.files[0]
+function readFile(e) {
+    const files = e.target.files
+    const imgFiles = []
+    for (let i = 0; i < files.length; i++) {
+        if (files[i].type.includes('image')) {
+            imgFiles.push(files[i])
+        }
+    }
+    
+    const numberOfFiles = imgFiles.length
+    const numberOfLiElements = liElement.parentElement.children.length
+    
+    if (numberOfFiles > 0 && numberOfFiles > numberOfLiElements) {
+        liElement.style.display = 'block'
+        for (let j = (numberOfLiElements + 1); j <= numberOfFiles; j++) {
+            let cloneLiElement = liElement.cloneNode(true)
+            liElement.parentElement.appendChild(cloneLiElement)
+        }
+    }
 
-    if(file && file.type.includes('image')){
+    for (let k = 0; k < imgFiles.length; k++) {
+
+        const liElementItem = liElement.parentElement.children[k]
         const reader = new FileReader()
 
-        reader.onload = function(readerEvent) {
-            
-            liElements.style.display = 'block'
-            
+        reader.onload = function (readerEvent) {
+
             const imgSrc = readerEvent.target.result
-            const imgName = file.name
-            const imgSize = file.size
+            const imgName = imgFiles[k].name
+            const imgSize = imgFiles[k].size
 
-            imgElement.setAttribute('src', imgSrc)
-            imgElementName.innerText = 'File name: ' + imgName
-            imgElementSize.innerText = 'Size: ' + (Math.round(imgSize / 1024 / 1024 *100))/100 + 'MB'
-
-
-            console.log(imgSrc)
+            liElementItem.children[0].innerText = 'File name: ' + imgName
+            liElementItem.children[1].setAttribute('src', imgSrc)
+            liElementItem.children[2].innerText = 'Size: ' + (Math.round(imgSize / 1024 / 1024 * 100)) / 100 + 'MB'
         }
-        reader.readAsDataURL(file)
+        reader.readAsDataURL(imgFiles[k])
+
+
     }
 }
