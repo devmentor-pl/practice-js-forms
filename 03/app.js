26547d0132f6ae4e7b0ee37bfcff283e10acd2d4
Karolina Grzywa <grzywakarolina@gmail.com>
2022-08-04 17:29:42 +0200

    Add task 3

--- 03/app.js
+++ 03/app.js
@@ -1,21 +1,36 @@
 const inputEl = document.querySelector('input');
 
 if (inputEl) {
-    inputEl.addEventListener('change', showImages)
+    inputEl.addEventListener('change', handleChange)
 }
 
-function showImages(e) {
-    const imagesList = document.querySelector('.images-list')
+function handleChange(e) {
+    const ulEl = document.querySelector('.images-list')
     const imagePrototypeEl = document.querySelector('.images-list__item--prototype')
-    const files = Array.from(e.target.files)
+    const files = Array.from(e.target.files);
 
     files.forEach(function (file) {
+        if (file.type.indexOf('image') >= 0) {
 
-        if (file.type.includes('image')) {
-            alert('choosen file IS an image')
+            const liEl = imagePrototypeEl.cloneNode(true);
+            const headerEl = liEl.querySelector('header');
+            const footerEl = liEl.querySelector('footer');
+            const imgEl = liEl.querySelector('img');
 
+            const fileReader = new FileReader();
+            fileReader.addEventListener('load', function (e) {
+                headerEl.innerText = file.name;
+                footerEl.innerText = (file.size / (1024 * 1024)).toFixed(2) + ' MB';
+                liEl.classList.remove('images-list__item--prototype');
+                ulEl.appendChild(liEl);
+                const image = e.target.result;
+                imgEl.src = image;
+            });
+
+            fileReader.readAsDataURL(file);
         } else {
-            alert('choosen file is NOT an image')
+            alert('Loaded file ' + file.name + ' is not an image type');
         }
+
     })
 }
