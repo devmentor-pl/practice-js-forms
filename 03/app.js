2972b02c826db786d635a052c15a3802f70f3630
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-09-15 17:25:37 +0200

    Add readFile function

---
+++ 03/app.js
@@ -0,0 +1,40 @@
+const fileEl = document.querySelector('input');
+const itemPrototype = document.querySelector('.images-list__item--prototype');
+const list = document.querySelector('.images-list');
+
+fileEl.addEventListener('change', readFile);
+
+function readFile(e){
+    e.preventDefault();
+    const files = e.target.files;
+    
+    for(let file of files){
+        
+        console.log(file.type);
+
+        if(file && file.type.includes('image')){
+            console.log(file.size);
+            console.log(file.name);
+            console.log(file.type);
+            const reader = new FileReader();
+
+            reader.onload = function(readerEvent){
+
+                const clone = itemPrototype.cloneNode(true);
+                const cloneName = clone.querySelector('.images-list__item-name');
+                const cloneImg = clone.querySelector('.images-list__item-img');
+                const cloneSize = clone.querySelector('.images-list__item-size');
+                clone.classList.remove('images-list__item--prototype');
+                const imgSrc = readerEvent.target.result;
+                cloneName.innerText = file.name;
+                console.log(file.size);
+                cloneSize.innerText = (file.size /(1024*1024)).toFixed(2) + "MB";
+                cloneImg.setAttribute('src', imgSrc);
+                list.appendChild(clone);
+            }
+            reader.readAsDataURL(file);
+        }
+    }
+
+
+}
