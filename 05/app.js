592f2609fd69cf7dd39327bd9ff242dec028d776
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-09-20 16:05:19 +0200

    Change checkData function

--- 05/app.js
+++ 05/app.js
@@ -1,75 +1,97 @@
 const form = document.querySelector('form');
 const messageList = form.querySelector('.messages');
-form.addEventListener('submit', checkData)
+
+if(form){
+    form.addEventListener('submit', checkData)
+}
 
 function checkData(e){
+    e.preventDefault();
+    const errors = [];
+    const fields = [
+        {
+            name: 'firstName',
+            label: 'Imię',
+            required: true,
+            pattern: '^[a-zA-Z –-]+$',
+        },
+        {
+            name: 'lastName',
+            label: 'Nazwisko',
+            required: true,
+            pattern: '^[a-zA-Z –-]+$',
+        },
+        {
+            name: 'street',
+            label: 'Ulica',
+            required: true,
+        },
+        {
+            name: 'houseNumber',
+            label: 'Numer budynku',
+            required: true,
+            type: 'number',
+        },
+        {
+            name: 'flatNumber',
+            label: 'Numer mieszkania',
+            type: 'number',
+        },
+        {
+            name: 'zip',
+            label: 'Kod pocztowy',
+            required: true,
+            pattern: '[0-9]{2}-[0-9]{3}',
+        },
+        {
+            name: 'city',
+            label: 'Miejscowość',
+            required: true,
+            pattern: '^[a-zA-Z –-]+$',
+        },
+        {
+            name: 'voivodeship',
+            label: 'Województwo',
+            required: true,
+        },
+    ];
+
+    fields.forEach(function(field){
+        const {name, label, required, type, pattern} = field;
+
+        const value = form.elements[name].value;
+
+        if(required){
+            if(value.length === 0){
+                errors.push('Field ' + label + ' is required!');
+            }
+        }
+        if(type === 'number'){
+            if(Number.isNaN(Number(value))){
+                errors.push('Field ' + label + ' contains invalid number!');
+            }
+        }
+        if(pattern){
+            const pattern = new RegExp(pattern);
+            if(!pattern.test(value)){
+                errors.push('Field ' + label + ' is invalid!');
+            }
+        }
+    })
+
     messageList.innerHTML = '';
-    const fields = Array.from(e.target.elements);
-    const firstNameInput = e.target.elements.firstName;
-    const lastNameInput = e.target.elements.lastName;
-    const streetInput = e.target.elements.street;
-    const houseInput = e.target.elements.houseNumber;
-    const flatInput = e.target.elements.flatNumber;
-    const zipInput = e.target.elements.zip;
-    const cityInput = e.target.elements.city;
-    const zipPattern = new RegExp(`${zipInput.pattern}`);
-    const isMatch = zipPattern.test(zipInput.value);
-
-    if(fields.some(element=>element.required && element.value.length <= 0)){
-        invalidValue()
-    }
-    for(element of fields){
-            element.style.border = 'none';
-        if(element.required && element.value.length <= 0){
-            element.style.border = '1px solid red';
-        }
-    }
     
-    if(/\d/.test(firstNameInput.value)){
-        const element = 'First name';
-        invalidValue(element, firstNameInput.value);
-    }
-    if(/\d/.test(lastNameInput.value)){
-        const element = 'Last name';
-        invalidValue(element, lastNameInput.value);
-    }
-    if(/\d/.test(streetInput.value)){
-        const element = 'Street name';
-        invalidValue(element, streetInput.value);
-    }
-    if(flatInput.value <=0 && flatInput.value.length !== 0){
-        const element = 'Flat number';
-        invalidValue(element, parseInt(flatInput.value));
-    }
-    if(houseInput.value <=0){
-        const element = 'House number';
-        invalidValue(element, parseInt(houseInput.value));
-    }
-    if(/\d/.test(cityInput.value)){
-        const element = 'City name';
-        invalidValue(element, cityInput.value);
-    }
-
-    if(zipInput.value.length > 6 || !isMatch){
-        const element = 'Zip number';
-        invalidValue(element, parseInt(zipInput.value));
-    }
-
-    function invalidValue(element, value){
-        const message = document.createElement('li');
-        if(typeof value === 'number'){
-            message.innerText = `${element} contains invalid number!`;
-        }else if(typeof value === 'string'){
-            message.innerText = `${element} contains invalid characters!`;
-        }else if(typeof value === 'undefined'){
-            message.innerText = `Marked fields are required`;
-            
-        }
-        messageList.appendChild(message);
-    }
-    if(messageList.innerHTML !== ''){
-        e.preventDefault();
-    }else{
+    if(errors.length === 0){
         alert('Provided data is valid');
+        fields.forEach(function(field){
+            form[field.name].value = '';
+        })
+    }else{
+        errors.forEach((function(error){
+            const liEl = document.createElement('li');
+            liEl.innerText = error;
+            
+            messageList.appendChild(liEl);
+        }))
     }
 }
