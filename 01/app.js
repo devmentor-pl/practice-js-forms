07cf4a9cdc62c0ae3ca12c225e73bdc557faf2cf
Jacek Zielinski <gandi34@wp.pl>
2022-10-18 07:35:02 +0200

    01 Js Forms Practice

---
+++ 01/app.js
@@ -0,0 +1,16 @@
+// W momencie wysyłania danych zatrzymać domyślną akcję foormularza 
+// oraz pobrać dane z jego pól
+
+const formEl = document.querySelector('form');
+formEl.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const first = e.target.elements.firstName;
+    const last = e.target.elements.lastName;
+    console.log(first.value);
+    console.log(last.value);
+});
+
+
+
+
