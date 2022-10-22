2f2a5a6ed702b5b23208d25999f5986296c21582
Jacek Zielinski <gandi34@wp.pl>
2022-10-22 12:24:35 +0200

    Js Forms

--- 05/app.js
+++ 05/app.js
@@ -12,9 +12,53 @@ if(formEl) {
 
 function stopSubmit(e) {
     e.preventDefault();
-}
+
+
 
 
 const nameEl = formEl.elements.firstName;
-const lastNAmeEl = formEl.elements.lastName;
-const streetEl = formEl.elements.streetName;
+const lastNameEl = formEl.elements.lastName;
+const streetEl = formEl.elements.street;
+const houseNr = formEl.elements.houseNumber;
+const flatNr = formEl.elements.flatNumber;
+const zipNr = formEl.elements.zip;
+const cityName = formEl.elements.city;
+const voivodeshipName = formEl.elements.voivodeship;
+
+const errors = [];
+
+if(nameEl.value.lenght === 0) {
+    errors.push('Error');
+}
+
+if(lastNameEl.value.lenght === 0) {
+    errors.push('Error Last');
+}
+
+if(streetEl.value.lenght === 0) {
+    errors.push('Error Street ');
+}
+
+if(houseNr.value.lenght === 0) {
+    errors.push('Error House ');
+}
+
+if(flatNr.value.lenght === 0) {
+    errors.push('Error Flat ');
+}
+
+if(zipNr.value.lenght === 0) {
+    errors.push('Error Zip ');
+}
+
+if(cityName.value.lenght === 0) {
+    errors.push('Error City ');
+}
+
+if(voivodeshipName.value.lenght === 0) {
+    errors.push('Error voivodeship ');
+}
+
+console.log(errors);
+};
+
