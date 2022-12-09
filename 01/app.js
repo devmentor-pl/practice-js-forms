de38cc5338c7d8f317d73b4952db771a16663c9d
Anna <aniamatrackasadurek@gmail.com>
2022-12-09 10:02:39 +0100

    exercise 1 finished + changes

--- 01/app.js
+++ 01/app.js
@@ -10,15 +10,23 @@ function createUser(e) {
     const firstName = e.target.elements.firstName.value;
     const lastName = e.target.elements.lastName.value;
 
+    const errors = [];
+
     if(firstName.length === 0) {
-        alert('field required');
-    } else if (lastName.length === 0) {
-        alert('field required');
-    } else {
+        errors.push('field required');
+    }
+    if(lastName.length === 0) {
+        errors.push('field required');
+    } 
+    if(errors.length > 0) {
+        e.preventDefault();
         const userName = firstName + ' ' + lastName;
         const liEl = document.createElement('li');
         liEl.classList.add('user-list__person');
         liEl.innerText = userName;
         ulEl.appendChild(liEl);
+        liEl.style.color = 'red';
+    } else {
+        alert('Your form is completed correctly :) ');
     }
 }
