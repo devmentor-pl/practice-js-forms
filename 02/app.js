dd5d59755a06c4bac81c21c6c7b7858165dca7d3
Michal Lata <maxpoz3212@gmail.com>
2023-01-06 16:51:52 +0100

    Add form validate program

--- 02/app.js
+++ 02/app.js
@@ -1,51 +1,38 @@
 const formEl = document.querySelector('form');
 
-const printErrors = function (sel1, sel2) {
-    let errors = [];
-
-    sel1 ? errors.push(sel1) : false;
-    sel2 ? errors.push(sel2) : false;
-    if (errors.length > 0) {
-
-        errors.forEach(function (item) {
-            item.style.color = 'red';
-        })
-    } else {
-        console.log('done');
-    }
-    // TO DO: reset jeśli są poprawne dane
-}
-
-const checkLogin = function (login) {
-    if (login.value.length === 0 || !login.value.includes('@')) {
-        printErrors(login.previousElementSibling);
-    }
-}
-
-const checkPassword = function (password, passwordConfirm) {
-    if (password.value.length <= 6 || passwordConfirm.value !== password.value) {
-        printErrors(password.previousElementSibling, passwordConfirm.previousElementSibling);
-    }
-}
-
-const checkAcceptReg = function (acceptReg) {
-    if (!acceptReg.checked) {
-        printErrors(acceptReg.previousElementSibling);
-    }
-}
-
 const checkData = function (formElList) {
     const login = formElList.login;
     const password = formElList.pass1;
     const passwordConfirm = formElList.pass2;
     const acceptReg = formElList.accept;
 
-    const loginErr = checkLogin(login);
-    const passwordErr = checkPassword(password, passwordConfirm);
-    const acceptRegErr = checkAcceptReg(acceptReg);
+    const errors = [];
 
-    // console.log(loginErr);
-    // console.log(acceptReg);
+
+    if (login.value.length === 0 || !login.value.includes('@')) {
+        errors.push(login.previousElementSibling);
+    }
+
+    if (password.value.length <= 6) {
+        errors.push(password.previousElementSibling);
+    }
+
+    if (passwordConfirm.value !== password.value) {
+        errors.push(passwordConfirm.previousElementSibling);
+    }
+
+    if (!acceptReg.checked) {
+        errors.push(acceptReg.previousElementSibling);
+    }
+
+    if (errors.length > 0) {
+        errors.forEach(function (item) {
+            item.style.color = 'red';
+
+        })
+    }
+
+    return errors;
 }
 
 const userRegister = function (e) {
@@ -53,8 +40,11 @@ const userRegister = function (e) {
 
     const formElList = e.target.elements;
 
-    checkData(formElList)
-    // printErrors(null, null)
+    const errors = checkData(formElList);
+
+    if (errors.length === 0) {
+        console.log('done');
+    }
 }
 
 formEl.addEventListener('submit', userRegister)
