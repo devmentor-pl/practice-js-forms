d715c12b0ef347a243bf95e2a9fdb3ced2909777
Michal Lata <maxpoz3212@gmail.com>
2023-01-06 21:22:30 +0100

    Add change color function

--- 04/app.js
+++ 04/app.js
@@ -3,8 +3,27 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
-}
 
+    const colorInput = boxElement.previousElementSibling.children[0];
+    colorInput.addEventListener('change', changeColor)
+    const rangeInput = boxElement.previousElementSibling.children[1];
+    rangeInput.addEventListener('mousemove', changeShadow);
+    rangeInput.addEventListener('change', changeShadow);
+
+
+    function changeColor(e) {
+        setBoxShadow(boxElement, e.target.value)
+    }
+
+    function changeShadow(e) {
+        const isMouseMoveEvent = e.type === 'mousemove';
+        const isMouseLeftButtonPress = e.buttons === 1;
+        if (isMouseMoveEvent && isMouseLeftButtonPress || !isMouseMoveEvent) {
+            const range = e.target.value / 100;
+            // setBoxShadow(boxElement, null, range) NIE DZIAŁA
+        }
+    }
+}
 function setBoxShadow(element, colorInHex, opacity = 1) {
     const colorInRGBA = `rgba(
         ${getChannelColor(colorInHex, 'red')}, 
@@ -19,7 +38,7 @@ function setBoxShadow(element, colorInHex, opacity = 1) {
 
 function getChannelColor(colorInHex, channelName) {
     let start;
-    switch(channelName) {
+    switch (channelName) {
         case 'red':
             start = 1;
             break;
@@ -33,8 +52,6 @@ function getChannelColor(colorInHex, channelName) {
 
     const channelColorHex = colorInHex.substr(start, 2);
     const channelColorDec = parseInt(channelColorHex, 16);
-
-    return channelColorDec; 
+    return channelColorDec;
 }
-
 
