9d7dda745ad09a8c0f0ed63e9155759f0cfa1a70
Michal Lata <maxpoz3212@gmail.com>
2023-01-09 16:17:59 +0100

    Add code rafactoring in task 01

--- 01/app.js
+++ 01/app.js
@@ -1,39 +1,50 @@
 const formEl = document.querySelector('form');
 
 
-const addUserToList = function (firstName, lastName) {
+const checkData = function (currElList, inputArr) {
+    for (let i = 0; i < currElList.length - 1; i++) {
+        const currEl = currElList[i];
+        const check = isNotEmpty(currEl);
+
+        if (check) {
+            inputArr.push(currEl.value);
+            clearInput(currEl);
+        }
+    }
+}
+
+const isNotEmpty = function (currEl) {
+    if (currEl.value.length === 0) {
+        alert(`Wprowadziłeś błędne dane w polu ${currEl.previousSibling.textContent.trim().slice(0, -1)}`);
+    } else {
+        return true;
+    }
+}
+
+const printData = function (inputArr) {
     const usersList = document.querySelector('.users-list');
     const listItem = document.createElement('li');
     listItem.classList.add('users-list__person');
 
-    listItem.innerText = `${firstName} ${lastName}`;
+    listItem.innerText = `${inputArr.join(' ')}`;
 
     usersList.appendChild(listItem);
 }
 
-const checkData = function (firstName, lastName) {
-    if (firstName.value.length === 0 || lastName.value.length === 0) {
-        alert('Wprowadziłeś błędne dane!')
-    } else {
-        addUserToList(firstName.value, lastName.value);
+const clearInput = function (currEl) {
+    currEl.value = '';
+}
+
+const getData = function (e) {
+    e.preventDefault();
+
+    const currElList = this.elements;
+    const inputArr = [];
+
+    checkData(currElList, inputArr)
+    if (inputArr.length === currElList.length - 1) {
+        printData(inputArr);
     }
 }
 
-const clearInput = function (firstName, lastName) {
-    firstName.value = '';
-    lastName.value = '';
-}
-
-const printData = function (e) {
-    e.preventDefault();
-
-    const firstName = formEl.elements['firstName'];
-    const lastName = formEl.elements['lastName'];
-
-    checkData(firstName, lastName);
-    clearInput(firstName, lastName);
-}
-
-formEl.addEventListener('submit', printData)
-
-
+formEl.addEventListener('submit', getData);
