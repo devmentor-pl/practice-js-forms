171299a9feaa142f89c9e313208c1ccb235a2135
Michal Lata <maxpoz3212@gmail.com>
2023-01-10 15:13:02 +0100

    Add fix in task 02

--- 02/app.js
+++ 02/app.js
@@ -1,50 +1,50 @@
 const formEl = document.querySelector('form');
 
+const showErrors = function (errorsList) {
+    errorsList.forEach(function (error) {
+        error.style.color = 'red';
+    })
+}
 const checkData = function (formElList) {
     const login = formElList.login;
     const password = formElList.pass1;
     const passwordConfirm = formElList.pass2;
     const acceptReg = formElList.accept;
+    const errorsList = [];
 
-    const errors = [];
 
+    for (const key of formElList) {
+        if (key.type !== "submit") {
+            key.previousElementSibling.style.color = 'black';
+        }
+    }
 
     if (login.value.length === 0 || !login.value.includes('@')) {
-        errors.push(login.previousElementSibling);
+        errorsList.push(login.previousElementSibling);
     }
 
     if (password.value.length <= 6) {
-        errors.push(password.previousElementSibling);
+        errorsList.push(password.previousElementSibling);
     }
 
     if (passwordConfirm.value !== password.value) {
-        errors.push(passwordConfirm.previousElementSibling);
+        errorsList.push(passwordConfirm.previousElementSibling);
     }
 
     if (!acceptReg.checked) {
-        errors.push(acceptReg.previousElementSibling);
+        errorsList.push(acceptReg.previousElementSibling);
     }
 
-    if (errors.length > 0) {
-        errors.forEach(function (item) {
-            item.style.color = 'red';
-
-        })
+    if (errorsList.length > 0) {
+        showErrors(errorsList);
+    } else {
+        console.log('done');
     }
-
-    return errors;
 }
 
 const userRegister = function (e) {
     e.preventDefault();
-
-    const formElList = e.target.elements;
-
-    const errors = checkData(formElList);
-
-    if (errors.length === 0) {
-        console.log('done');
-    }
+    checkData(e.target.elements);
 }
 
 formEl.addEventListener('submit', userRegister)
