14b808a6ef4adca207d1f01a776ccf966bb930d1
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2023-02-01 13:07:45 +0100

    01 fixed

--- 01/app.js
+++ 01/app.js
@@ -1,23 +1,18 @@
 const form = document.querySelector('form');
 const submit = document.querySelector('input[type=submit]');
 
-const getValue = function(e) {
-    e.preventDefault();
-    let liContent = '';
-    const ul = document.querySelector('.users-list');
-    for(const el of form.elements){
-      if(el.getAttribute('type') != 'submit'){
-        liContent = `${liContent} ${el.value}`
-        el.value = "";
-      }   
-    }
-    const li = document.createElement('li');
-    li.classList.add('user-list__person');
-    li.textContent = liContent;
+const getValue = function (e) {
+  e.preventDefault();
+  let liContent = '';
+  const ul = document.querySelector('.users-list');
+  liContent = document.querySelector('input[name="firstName"]').value + " " + document.querySelector('input[name="lastName"]').value;
+  const li = document.createElement('li');
+  li.classList.add('user-list__person');
+  li.textContent = liContent;
+  if (liContent != " ") {
     ul.appendChild(li);
-    console.log(liContent);
+  }
 }
-if(form)
-{
-    submit.addEventListener('click', getValue);
+if (form) {
+  submit.addEventListener('click', getValue);
 }
