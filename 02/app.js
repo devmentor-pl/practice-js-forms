d488b1df625ee3009a4b43256b675ff0c6564cf8
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-02-10 14:27:10 +0100

    Fix mistake in ex 2

--- 02/app.js
+++ 02/app.js
@@ -25,19 +25,19 @@ function register(e) {
 	const errors = [];
 
 	if (!emailElementValue.includes("@")) {
-		errors.push("emailElement");
+		errors.push(emailElement);
 	}
 
 	if (password1ElementValue !== password2ElementValue) {
-		errors.push("password1Element", "password2Element");
+		errors.push(password1Element, password2Element);
 	}
 
 	if (password1ElementValue.length || password2ElementValue.length <= 6) {
-		errors.push("password1Element", "password2Element");
+		errors.push(password1Element, password2Element);
 	}
 
 	if (!checkedElement.checked) {
-		errors.push("checkedElement");
+		errors.push(checkedElement);
 	}
 
 	console.log(errors);
