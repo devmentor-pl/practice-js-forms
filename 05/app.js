e1f01f0d5f4faf155833a7949c8902194ddbada1
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-02-14 12:43:39 +0100

    Refacture ex 5

--- 05/app.js
+++ 05/app.js
@@ -1,73 +1,139 @@
 document.addEventListener("DOMContentLoaded", init);
+const formElement = document.querySelector("form");
 
 function init() {
-	const formElement = document.querySelector("form");
 	formElement.addEventListener("submit", handleData);
 }
 
-function handleData(e) {
-	e.preventDefault();
-	const wrongDataList = document.querySelector(".messages");
+if (formElement) {
+	function handleData(e) {
+		e.preventDefault();
+		const wrongDataList = document.querySelector(".messages");
+		let errors = [];
 
-	let errors = [];
+		const fields = [
+			{
+				name: "firstName",
+				label: "Imię",
+				required: true,
+			},
+			{
+				name: "lastName",
+				label: "Nazwisko",
+				required: true,
+			},
+			{ name: "street", label: "Ulica", required: true },
+			{
+				name: "houseNumber",
+				label: "Numer budynku",
+				type: "number",
+				required: true,
+			},
+			{ name: "flatNumber", label: "Numer mieszkania", type: "number" },
+			{
+				name: "zip",
+				label: "Kod pocztowy",
+				pattern: "^[0-9]{2}-[0-9]{3}$",
+				required: true,
+			},
+			{
+				name: "city",
+				label: "Miejscowość",
+				required: true,
+			},
+			{ name: "voivodeship", label: "Województwo", required: true },
+		];
 
-	const firstNameElement = document.querySelector('input[name="firstName"]');
-	const lastNameElement = document.querySelector('input[name="lastName"]');
-	const streetElement = document.querySelector('input[name="street"]');
-	const houseNumberElement = document.querySelector(
-		'input[name="houseNumber"]'
-	);
-	const flatNumberElement = document.querySelector('input[name="flatNumber"]');
-	const zipElement = document.querySelector('input[name="zip"]');
-	const cityElement = document.querySelector('input[name="city"]');
-	const voivodeshipElement = document.querySelector(
-		'select[name="voivodeship"]'
-	);
+		fields.forEach(function (field) {
+			const value = formElement.elements[field.name].value;
 
-	if (firstNameElement.value === "") {
-		errors.push("Podaj swoje imię");
-	}
+			if (field.required) {
+				if (value.length === 0) {
+					errors.push("To pole jest wymagane.");
+				}
+			}
 
-	if (lastNameElement.value === "") {
-		errors.push("Podaj swoje nazwisko");
-	}
+			if (field.type === "number") {
+				if (Number.isNaN(Number(value))) {
+					errors.push("W tym polu wpisz liczbę.");
+				}
+			}
 
-	if (streetElement.value === "") {
-		errors.push("Wpisz swoją ulicę");
-	}
+			if (field.pattern) {
+				const reg = new RegExp(field.pattern);
 
-	if (
-		houseNumberElement.value === "" ||
-		Number.isNaN(houseNumberElement.value)
-	) {
-		errors.push("Podaj numer domu");
-	}
+				if (!reg.test(value)) {
+					errors.push("Wpisz kod pocztowy zgodnie ze wzorem.");
+				}
+			}
+		});
 
-	if (flatNumberElement.value === "" || Number.isNaN(flatNumberElement.value)) {
-		errors.push("Podaj numer mieszkania");
-	}
+		// const firstNameElement = document.querySelector('input[name="firstName"]');
+		// const lastNameElement = document.querySelector('input[name="lastName"]');
+		// const streetElement = document.querySelector('input[name="street"]');
+		// const houseNumberElement = document.querySelector(
+		// 	'input[name="houseNumber"]'
+		// );
+		// const flatNumberElement = document.querySelector(
+		// 	'input[name="flatNumber"]'
+		// );
+		// const zipElement = document.querySelector('input[name="zip"]');
+		// const cityElement = document.querySelector('input[name="city"]');
+		// const voivodeshipElement = document.querySelector(
+		// 	'select[name="voivodeship"]'
+		// );
 
-	if (zipElement.value === "" || !/[0-9]{2}-[0-9]{3}/.test(zipElement.value)) {
-		errors.push("Podaj prawidłlowy kod");
-	}
+		// if (firstNameElement.value === "") {
+		// 	errors.push("Podaj swoje imię");
+		// }
 
-	if (cityElement.value === "") {
-		errors.push("Wybierz swoje miasto");
-	}
+		// if (lastNameElement.value === "") {
+		// 	errors.push("Podaj swoje nazwisko");
+		// }
 
-	if (voivodeshipElement.value === "") {
-		errors.push("Wybierz swoje województwo");
-	}
+		// if (streetElement.value === "") {
+		// 	errors.push("Wpisz swoją ulicę");
+		// }
 
-	if (errors.length === 0) {
-		alert("Dziękujemy za wypełnienie formularza");
-	} else {
+		// if (
+		// 	houseNumberElement.value === "" ||
+		// 	Number.isNaN(houseNumberElement.value)
+		// ) {
+		// 	errors.push("Podaj numer domu");
+		// }
+
+		// if (
+		// 	flatNumberElement.value === "" ||
+		// 	Number.isNaN(flatNumberElement.value)
+		// ) {
+		// 	errors.push("Podaj numer mieszkania");
+		// }
+
+		// if (
+		// 	zipElement.value === "" ||
+		// 	!/[0-9]{2}-[0-9]{3}/.test(zipElement.value)
+		// ) {
+		// 	errors.push("Podaj prawidłlowy kod");
+		// }
+
+		// if (cityElement.value === "") {
+		// 	errors.push("Wybierz swoje miasto");
+		// }
+
+		// if (voivodeshipElement.value === "") {
+		// 	errors.push("Wybierz swoje województwo");
+		// }
+
 		wrongDataList.innerHTML = " ";
 
-		errors.forEach(function (err) {
-			const liEl = document.createElement("li");
-			liEl.innerText = err;
-			wrongDataList.appendChild(liEl);
-		});
+		if (errors.length === 0) {
+			alert("Dziękujemy za wypełnienie formularza");
+		} else {
+			errors.forEach(function (err) {
+				const liEl = document.createElement("li");
+				liEl.innerText = err;
+				wrongDataList.appendChild(liEl);
+			});
+		}
 	}
 }
