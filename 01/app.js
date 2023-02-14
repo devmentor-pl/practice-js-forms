3a76baa949981bb6ee58ec2b22115f8e5ba15635
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-02-14 09:55:36 +0100

    Fix ex 1

--- 01/app.js
+++ 01/app.js
@@ -1,18 +1,22 @@
 const formEl = document.querySelector("form");
+const ulElement = document.querySelector("ul");
 
 formEl.addEventListener("submit", getUserData);
 
 function getUserData(e) {
 	e.preventDefault();
 
-	const firstName = document.querySelector('input[name="firstName"]').value;
-	const lastName = document.querySelector('input[name="lastName"]').value;
+	const firstName = document.querySelector('input[name="firstName"]');
+	const lastName = document.querySelector('input[name="lastName"]');
 
-	const ulElement = document.querySelector("ul");
-	const liElement = document.createElement("li");
-	liElement.classList.add("user-list__person");
-
-	liElement.innerText = firstName + " " + lastName;
-
-	ulElement.appendChild(liElement);
+	if (firstName.value !== "" && lastName.value !== "") {
+		const liElement = document.createElement("li");
+		liElement.classList.add("user-list__person");
+		liElement.innerText = firstName.value + " " + lastName.value;
+		ulElement.appendChild(liElement);
+		firstName.value = "";
+		lastName.value = "";
+	} else {
+		alert("Proszę wypełnić pola!");
+	}
 }
