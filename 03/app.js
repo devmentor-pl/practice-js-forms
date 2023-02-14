a74ccac89e7611b474214596817e03f683e4e36e
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-02-14 10:59:55 +0100

    Fix ex 3 but reader.onload is not working

--- 03/app.js
+++ 03/app.js
@@ -9,24 +9,29 @@ function addFiles(e) {
 
 	filesListArray.forEach(function (file) {
 		if (file && file.type.includes("image")) {
-			const imgPrototypeElement = document.querySelector(
-				".images-list__item--prototype"
-			);
-			const imgElement = imgPrototypeElement.cloneNode(true);
-			imgElement.classList.remove("images-list__item--prototype");
-
-			imgElement.children[0].textContent = file.name;
-			imgElement.children[2].textContent =
-				(Number(file.size) / (1024 * 1024)).toFixed(2) + "MB";
-
-			imagesList.appendChild(imgElement);
+			createIMGElement(file);
 
 			const reader = new FileReader();
 
-			reader.onload = function (readerEvent) {
+			reader.onload = function (readerEvent, imgElement) {
 				imgElement.children[1].setAttribute("src", readerEvent.target.result);
 			};
 			reader.readAsDataURL(file);
 		}
 	});
 }
+
+const createIMGElement = function (fileExample) {
+	const imgPrototypeElement = document.querySelector(
+		".images-list__item--prototype"
+	);
+	const imgElement = imgPrototypeElement.cloneNode(true);
+	imgElement.classList.remove("images-list__item--prototype");
+
+	imgElement.children[0].textContent = fileExample.name;
+	imgElement.children[2].textContent =
+		(Number(fileExample.size) / (1024 * 1024)).toFixed(2) + "MB";
+
+	imagesList.appendChild(imgElement);
+	return imgElement;
+};
