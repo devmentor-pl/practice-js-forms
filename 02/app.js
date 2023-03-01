11bb0922ff13b2719fa484af7b5507a43f7b8fa2
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-03-01 10:02:34 +0100

    update task-02

--- 02/app.js
+++ 02/app.js
@@ -1,29 +1,35 @@
 const formEl = document.querySelector('form')
 const errors = []
-console.log(errors)
 
 formEl.addEventListener('submit', function (e) {
 	e.preventDefault()
 
-	checkEmail(e)
-	checkPassword(e)
+	isCorrectEmail(e)
+	isCorrectPassword(e)
 	acceptRegulations(e)
 	showErrors(e)
 })
 
-function checkEmail(e) {
-	const re = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,4}$/i
+function isCorrectEmail(e) {
 	const inpEmail = e.target.elements['login']
 	const email = inpEmail.value
-	const validEmail = email.match(re)
-	if (validEmail) {
+
+	if (emailValidation(email)) {
 		clearErrors(inpEmail)
 	} else {
 		errors.push(inpEmail)
 	}
 }
 
-function checkPassword(e) {
+function emailValidation(email) {
+	const re = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,4}$/i
+	const validEmail = email.match(re)
+	if (validEmail) {
+		return true
+	}
+}
+
+function isCorrectPassword(e) {
 	const inpPass1 = e.target.elements['pass1']
 	const inpPass2 = e.target.elements['pass2']
 	const pass1 = inpPass1.value
