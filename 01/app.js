54e86fba508c6c3281eb94e98f42120a8b0fb6e2
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-03-01 09:25:24 +0100

    update task-01

--- 01/app.js
+++ 01/app.js
@@ -10,18 +10,22 @@ function getUser(e) {
 	const lastName = e.target.elements.lastName
 
 	if (firstName.value !== '' && lastName.value !== '') {
-		addUser(firstName, lastName)
+		addUserToList(firstName, lastName)
+		clearInputs(firstName, lastName)
 	} else {
 		alert('Wprowadź wszystkie dane')
 	}
 }
 
-function addUser(firstName, lastName) {
+function addUserToList(firstName, lastName) {
 	const usersList = document.querySelector('.users-list')
 	const newUser = document.createElement('li')
 	newUser.classList.add('users-list__person')
 	newUser.textContent = `${firstName.value} ${lastName.value}`
 	usersList.appendChild(newUser)
+}
+
+function clearInputs(firstName, lastName) {
 	firstName.value = ''
 	lastName.value = ''
 }
