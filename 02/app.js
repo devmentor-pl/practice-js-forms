40eda37589ddf94d20bf6dce8fc8d90657015097
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-05-08 20:18:41 +0800

    add event listener on submit with functions to check validity

--- 02/app.js
+++ 02/app.js
@@ -5,25 +5,66 @@ const acceptInput = form.querySelector('input[type="checkbox"]');
 const errors = [];
 form.setAttribute("novalidate", "");
 
-form.addEventListener("submit", function (e) {
-  //check if email is valid
+//check if email is valid
+const checkEmail = function () {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(emailInput.value)) {
-    errors.push(emailInput);
+    if (errors.indexOf(emailInput) === -1) {
+      errors.push(emailInput);
+    }
+  } else {
+    //remove error
+    errors.splice(errors.indexOf(emailInput), 1);
+    setLabelColorToBlack(emailInput);
   }
-  //check if passwords are the same
+};
+
+//check if passwords match
+const checkPasswords = function () {
   const password1 = passwordInputs[0].value;
   const password2 = passwordInputs[1].value;
-  if (password1.length < 6 || password1 !== password2) {
-    errors.push(...passwordInputs);
+  console.log("passw if: ", password1.length < 6 || password1 !== password2);
+  if (
+    password1.length < 6 ||
+    password1 !== password2 ||
+    password1 !== "" ||
+    password2 !== ""
+  ) {
+    if (errors.indexOf(passwordInputs[0]) === -1) {
+      errors.push(...passwordInputs);
+    }
+  } else {
+    const password1Index = errors.indexOf(passwordInputs[0]);
+    const password2Index = errors.indexOf(passwordInputs[1]);
+    errors.splice(errors.indexOf(password1Index), 1);
+    errors.splice(errors.indexOf(password2Index), 1);
+    setLabelColorToBlack(passwordInputs[0]);
+    setLabelColorToBlack(passwordInputs[1]);
   }
+};
 
-  // check if terms are accepted
+//check if terms are accepted
+const areTermsAccepted = function () {
   if (!acceptInput.checked) {
-    errors.push(acceptInput);
+    if (errors.indexOf(acceptInput) === -1) {
+      errors.push(acceptInput);
+    }
+  } else {
+    errors.splice(errors.indexOf(acceptInput), 1);
+    setLabelColorToBlack(acceptInput);
   }
+};
 
-  //check errors
+const setLabelColorToBlack = function (element) {
+  const label = document.querySelector(`label[for="${element.id}"]`);
+  label.style.color = "black";
+};
+
+form.addEventListener("submit", function (e) {
+  checkEmail();
+  checkPasswords();
+  areTermsAccepted();
+  //color tags marked as errors
   if (errors.length > 0) {
     e.preventDefault();
     errors.forEach(function (error) {
@@ -35,102 +76,11 @@ form.addEventListener("submit", function (e) {
   }
 });
 
-form.addEventListener("change", function (e) {
-  const errorIndex = errors.indexOf(e.target);
-  console.log("errorIndex: ", errorIndex);
-  if (errorIndex >= 0) {
-    const label = document.querySelector(`label[for="${e.target.id}"]`);
-    console.log("label: ", label);
-    label.style.color = "black";
-  }
-  // console.log("change: ", e.target);
-  // console.log("index", errors.indexOf(e.target));
-  // errors = errors.filter((element, index) => console.log("element", element));
-});
-
-// const form = document.querySelector("form");
-// const emailInput = form.querySelector('input[type="email"]');
-// const passwordInputs = form.querySelectorAll('input[type="password"]');
-// const acceptInput = form.querySelector('input[type="checkbox"]');
-
-// form.setAttribute("novalidate", "");
-// form.addEventListener("submit", (event) => {
-//   const errors = [];
-//   // Sprawdzenie adresu e-mail
-//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//   if (!emailRegex.test(emailInput.value)) {
-//     errors.push(emailInput);
-//   }
-
-//   // Sprawdzenie haseł
-//   const password1 = passwordInputs[0].value;
-//   const password2 = passwordInputs[1].value;
-//   if (password1.length < 6 || password1 !== password2) {
-//     errors.push(...passwordInputs);
-//   }
-
-//   // Sprawdzenie akceptacji regulaminu
-//   if (!acceptInput.checked) {
-//     errors.push(acceptInput);
-//   }
-
-//   // Obsługa błędów
-//   if (errors.length > 0) {
-//     event.preventDefault();
-
-//     errors.forEach((input) => {
-//       const label = form.querySelector(`label[for="${input.id}"]`);
-//       label.style.color = "red";
-//     });
-//   } else {
-//     console.log("done");
-//   }
-// });
-
-// // Dodawanie i usuwanie błędów
-// const addError = (input) => {
-//   if (!errors.includes(input)) {
-//     errors.push(input);
-//     const label = form.querySelector(`label[for="${input.id}"]`);
-//     label.style.color = "red";
-//   }
-// };
-
-// const removeError = (input) => {
-//   const index = errors.indexOf(input);
-//   if (index !== -1) {
-//     errors.splice(index, 1);
-//     const label = form.querySelector(`label[for="${input.id}"]`);
+// form.addEventListener("change", function (e) {
+//   const errorIndex = errors.indexOf(e.target);
+//   console.log("errorIndex: ", errorIndex);
+//   if (errorIndex >= 0) {
+//     const label = document.querySelector(`label[for="${e.target.id}"]`);
 //     label.style.color = "black";
 //   }
-// };
-
-// // Obsługa zdarzeń
-// emailInput.addEventListener("input", checkEmail);
-// passwordInputs[0].addEventListener("input", checkPasswords);
-// passwordInputs[1].addEventListener("input", checkPasswords);
-// acceptInput.addEventListener("change", checkAccept);
-
-// form.addEventListener("submit", (event) => {
-//   checkEmail();
-//   checkPasswords();
-//   checkAccept();
-
-//   if (errors.length > 0) {
-//     event.preventDefault();
-//   } else {
-//     console.log("done");
-//   }
 // });
-
-// // Usuwanie błędów
-// const clearErrors = () => {
-//   errors.forEach((input) => {
-//     removeError(input);
-//   });
-// };
-
-// emailInput.addEventListener("change", clearErrors);
-// passwordInputs[0].addEventListener("change", clearErrors);
-// passwordInputs[1].addEventListener("change", clearErrors);
-// acceptInput.addEventListener("change", clearErrors);
