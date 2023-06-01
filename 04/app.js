e2917b40f95f22aaffc74f063e3d33d57a9a08da
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-06-01 13:48:28 +0200

    Finish task 04

--- 04/app.js
+++ 04/app.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const boxElement = document.querySelector('.box');
+    const inputOpacity = document.querySelector('input[name="opacity"]')
+    inputOpacity.setAttribute('value', 0)
     setBoxShadow(boxElement, '#000000');
 }
 
@@ -19,7 +21,7 @@ function setBoxShadow(element, colorInHex, opacity = 1) {
 
 function getChannelColor(colorInHex, channelName) {
     let start;
-    switch(channelName) {
+    switch (channelName) {
         case 'red':
             start = 1;
             break;
@@ -34,7 +36,38 @@ function getChannelColor(colorInHex, channelName) {
     const channelColorHex = colorInHex.substr(start, 2);
     const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+    return channelColorDec;
 }
 
+const inputColor = document.querySelector('input[name="color"]')
+inputColor.addEventListener('change', changeColor)
+let newColor
+let opacity
+
+function changeColor(e) {
+    const boxElement = document.querySelector('.box');
+    newColor = e.target.value
+
+    setBoxShadow(boxElement, newColor, opacity)
+}
+
+const inputOpacity = document.querySelector('input[name="opacity"]')
+inputOpacity.addEventListener('change', changeOpacity)
+inputOpacity.addEventListener('mousemove', changeOpacity)
+
+function changeOpacity(e) {
+    const boxElement = document.querySelector('.box');
+    let rangeValue
+
+    if (!newColor) {
+        newColor = "#000000"
+    }
+    if (e.type === 'change' || e.type === "mousemove" && e.buttons === 1) {
+        rangeValue = e.target.value
+    }
+    if (rangeValue) {
+        opacity = (100 - rangeValue) / 100 // nie jestem pewna czy dobrze zrozumialam, ale zakładam że range = 0 to opacity = 1, dlatego zmieniłam początkowe value dla inputa (z 100 na 0). Nie wiem czy mogłam ingerować w funkcję init. :)
+        setBoxShadow(boxElement, newColor, opacity)
+    }
+}
 
