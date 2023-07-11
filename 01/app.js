ee61287b65abd62c0d8200d19ddd7d54f7a04a55
MarcinK <marcin.kulbicki@gmail.com>
2023-07-11 17:22:45 +0200

    refactor ex1

--- 01/app.js
+++ 01/app.js
@@ -1,8 +1,12 @@
 const formEl = document.querySelector("form");
 const usersListEl = document.querySelector(".users-list");
 
-const regexName =
-  /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+const regexName = /^[a-zA-Z –-]+$/u;
+const defaultLiOptionsKeys = ["className", "innerText"];
+const defaultLiElOptions = {
+  className: "",
+  innerText: "",
+};
 
 formEl.addEventListener("submit", handleSubmit);
 
@@ -20,23 +24,44 @@ function handleSubmit(e) {
 
 function validateInputEl(inputEl) {
   const isValid = regexName.test(inputEl.value.trim());
-  if (!isValid) throw new Error("Input is not valid");
+  if (!isValid) alert("Input is not valid");
 }
 
 function printData(...names) {
   const capitalizedNames = names.map((name) => {
     const isString = typeof name === "string";
-    if (!isString) throw new Error("not a string type");
+    if (!isString) alert("not a string type");
 
     const normaliseName = capitalizeName(name);
     return normaliseName;
   });
 
-  if (usersListEl && usersListEl.firstElementChild) {
-    const cloneLiEl = usersListEl.firstElementChild.cloneNode(true);
-    cloneLiEl.innerText = capitalizedNames.join(" ");
-    usersListEl.appendChild(cloneLiEl);
+  if (!usersListEl) return null;
+  const newLi = createLiEl({
+    className: "users-list__person",
+    innerText: capitalizedNames.join(" "),
+  });
+  usersListEl.appendChild(newLi);
+}
+
+function createLiEl(options = defaultLiElOptions) {
+  const isObject =
+    Object.prototype.toString.call(options) === "[object Object]" &&
+    !Array.isArray(options) &&
+    options !== null;
+  if (!isObject) alert("given options should be provided in type object");
+  const optionsKeys = Object.keys(options);
+  for (const optionKeyItem of optionsKeys) {
+    const isKeyValid = defaultLiOptionsKeys.includes(optionKeyItem);
+    if (!isKeyValid) alert("provided option key is not valid");
   }
+
+  const { className, innerText } = { ...defaultLiElOptions, ...options };
+  const newLiEl = document.createElement("li");
+  newLiEl.classList.add(className);
+  newLiEl.innerText = innerText;
+
+  return newLiEl;
 }
 
 function capitalizeName(name) {
