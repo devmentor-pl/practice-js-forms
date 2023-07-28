f4e3c7c1d246fa229a76e3d87fdf734249ab4548
anetasobanska <sannettka@gmail.com>
2023-07-28 15:16:34 +0200

    finish 05

---
+++ 05/app.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const form = document.querySelector('form');
+    const ulMessages = document.querySelector('.messages');
+
+    form.noValidate = true;
+
+    const fields = [
+    {
+      name: 'firstName',
+      label: 'Imię',
+      required: true,
+      pattern: '^[a-zA-Z –-]+$',
+    },
+    {
+      name: 'lastName',
+      label: 'Nazwisko',
+      required: true,
+      pattern: '^[a-zA-Z –-]+$',
+    },
+    { name: 'street', label: 'Ulica', required: true },
+    {
+      name: 'houseNumber',
+      label: 'Numer budynku',
+      type: 'number',
+      required: true,
+    },
+    { name: 'flatNumber', label: 'Numer mieszkania', type: 'number' },
+    {
+      name: 'zip',
+      label: 'Kod pocztowy',
+      pattern: '^[0-9]{2}-[0-9]{3}$',
+      required: true,
+    },
+    {
+      name: 'city',
+      label: 'Miejscowość',
+      required: true,
+      pattern: '^[a-zA-Z –-]+$',
+    },
+    { name: 'voivodeship', label: 'Województwo', required: true },
+  ];
+
+  form.addEventListener('submit', handleSubmit);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const errors = [];
+    
+    fields.forEach(function (field) {
+        const value = form.elements[field.name].value;
+        if (field.required) {
+            if (value.length === 0) {
+                errors.push('Dane w polu ' + field.label + ' są wymagane.');
+            }
+        }
+
+        if (field.type === 'number') {
+            if(typeof Number(value) !== 'number') {
+                errors.push('Dane w polu ' + field.label + ' musza byc liczba.');
+            }
+        }
+
+        if(field.pattern) {
+            const regex = new RegExp(field.pattern);
+            if(!regex.test(value)) {
+                errors.push('Dane w polu ' + field.label + ' nie sa poprawne. Wpisz wg wzoru 00-000');
+            }
+        }
+    })
+
+    ulMessages.innerText = '';
+    if(errors.length === 0) {
+        ulMessages.innerText = 'Dane zostaly wypelnione prawidlowo';
+    } else {
+        errors.forEach(err => {
+            const li = document.createElement('li');
+            li.innerText = err;
+            ulMessages.appendChild(li);
+        })
+    }
+
+  }
+}
