06b77ff544fcc163309891159792458e05c21453
Marcin <xcinek1337@wp.pl>
2023-08-08 14:14:27 +0200

    03

--- 03/app.js
+++ 03/app.js
@@ -9,28 +9,31 @@ function showInfoFiles(e) {
 
 	for (const file of files) {
 		if (file.type.includes('image')) {
-			const liElemClone = liElem.cloneNode(true);
-			liElemClone.classList.remove('images-list__item--prototype');
-
-			const fileName = liElemClone.querySelector('.images-list__item-name');
-			const fileNameWithoutExtension = file.name.split('.').slice(0, -1).join();
-			fileName.textContent = fileNameWithoutExtension;
-
-			const fileSize = liElemClone.querySelector('.images-list__item-size');
-			fileSize.textContent = (file.size / (1024 * 1024)).toFixed(2) + ' MB';
-
+			const readyLiElem = createInfo(file);
+			
 			const reader = new FileReader();
 			reader.onload = function (e) {
 				const imgSrc = e.target.result;
-				const picture = liElemClone.querySelector('.images-list__item-img');
+				const picture = readyLiElem.querySelector('.images-list__item-img');
 				picture.src = imgSrc;
 			};
-
 			reader.readAsDataURL(file);
-
-			ulList.appendChild(liElemClone);
+			ulList.appendChild(readyLiElem);
 		}
+		createInfo(file);
 	}
-	//usuwam ten pierwszy li("macierzysty szablon") bo nie jest juz potrzebny
-	liElem.remove();
+}
+
+function createInfo(file) {
+	const liElemClone = liElem.cloneNode(true);
+	liElemClone.classList.remove('images-list__item--prototype');
+
+	const fileName = liElemClone.querySelector('.images-list__item-name');
+	const fileNameWithoutExtension = file.name.split('.').slice(0, -1).join();
+	fileName.textContent = fileNameWithoutExtension;
+
+	const fileSize = liElemClone.querySelector('.images-list__item-size');
+	fileSize.textContent = (file.size / (1024 * 1024)).toFixed(2) + ' MB';
+	
+	return liElemClone;
 }
