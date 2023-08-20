dbda374f0cc51c8691d5950adb3f9cfaa1bd419d
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-08-20 23:15:09 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,12 +1,34 @@
 const formEl = document.querySelector("form");
+formEl.addEventListener('submit', handleSubmit)
 
-const handleChange = function (e) {
-  const login = formEl.elements.login;
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  
-};
-
-for (const el of formEl.elements) {
-  el.addEventListener("input", handleChange);
+function handleSubmit(e) {
+ e.preventDefault()
+ const email = e.target.elements.login;
+ const pass1 = e.target.elements.pass1; 
+ const pass2 = e.target.elements.pass2;     
+ const checkbox = e.target.elements.accept;
+ const errors = [];
+ if (!email.value.includes('@')) {
+    errors.push(email)
+ } 
+ if (pass1.value.length === 0 || pass1.value.length < 6) {
+    errors.push(pass1)
+ } 
+ if (pass2.value.length === 0 || pass2.value.length < 6)  {
+    errors.push(pass2)
+ }
+ if (pass1.value !== pass2.value) {
+    errors.push(pass1, pass2)
+ }
+ if (!checkbox.checked) {
+    errors.push(checkbox)
+ }  
+ errors.forEach(function(input) {
+    const labelError = input.previousElementSibling;
+    labelError.style.color = "red";
+ })
+ if (errors.length === 0) {
+    console.log('DONE!');
+ }
 }
+
