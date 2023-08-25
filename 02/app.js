2bd7098088c10ea223d0bcbb464f80654930b23b
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-08-25 22:17:34 +0200

    delete unnecesary if

--- 02/app.js
+++ 02/app.js
@@ -11,15 +11,9 @@ function handleSubmit(e) {
  if (!email.value.includes('@')) {
     errors.push(email)
  } 
- if (pass1.value.length === 0 || pass1.value.length < 6) {
-    errors.push(pass1);
- } 
- if (pass2.value.length === 0 || pass2.value.length < 6)  {
-    errors.push(pass2)
- }
- if (pass1.value !== pass2.value) {
-    errors.push(pass1, pass2)
- }
+if (pass1.value.length <= 6 && pass2.value.length <= 6 || pass1.value !== pass2.value) {
+   errors.push(pass1, pass2)
+}
  if (!checkbox.checked) {
     errors.push(checkbox)
  }  
@@ -29,6 +23,12 @@ function handleSubmit(e) {
  })
  if (errors.length === 0) {
     console.log('DONE!');
+    const inputs = [...formEl.elements];
+    inputs.forEach(function(input) {
+      input.value = "";
+    })
  }
+ 
+ 
 }
 
