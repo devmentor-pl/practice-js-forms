cec67d99e9f40d33b11ada0585b0174a4927fd89
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-08-28 23:07:38 +0200

    task 2 fixed

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,7 @@
 const formEl = document.querySelector("form");
 formEl.addEventListener('submit', handleSubmit)
 
+
 function handleSubmit(e) {
  e.preventDefault()
  const email = e.target.elements.login;
@@ -11,24 +12,35 @@ function handleSubmit(e) {
  if (!email.value.includes('@')) {
     errors.push(email)
  } 
+ else {
+    email.previousElementSibling.style.color = "black";
+ }
 if (pass1.value.length <= 6 && pass2.value.length <= 6 || pass1.value !== pass2.value) {
    errors.push(pass1, pass2)
 }
+else {
+    pass1.previousElementSibling.style.color = "black";
+    pass2.previousElementSibling.style.color = "black";
+ }
  if (!checkbox.checked) {
     errors.push(checkbox)
- }  
- errors.forEach(function(input) {
-    const labelError = input.previousElementSibling;
-    labelError.style.color = "red";
- })
+ }
+ else {
+    checkbox.previousElementSibling.style.color = "black";
+ }
  if (errors.length === 0) {
     console.log('DONE!');
-    const inputs = [...formEl.elements];
-    inputs.forEach(function(input) {
-      input.value = "";
-    })
+    const inputs = [...formEl.elements]
+    for (let i = 0; i < inputs.length - 1; i++) {
+        inputs[i].value = "";
+        inputs[i].checked = false;
+    }
  }
- 
- 
+ else {
+    errors.forEach(function(input) {
+        const labelError = input.previousElementSibling;
+        labelError.style.color = "red";
+     })
+ }
 }
 
