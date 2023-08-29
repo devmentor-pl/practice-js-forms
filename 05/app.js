d07e674aad9280a21097c5d32583b6c45fb4b833
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-08-29 13:41:14 +0200

    add array with fields objects && delete unnecesery if

--- 05/app.js
+++ 05/app.js
@@ -1,61 +1,67 @@
 const form = document.querySelector("form");
 const msgList = document.querySelector("ul");
-let msgArray = [];
 form.addEventListener("submit", handleSubmit);
+const fields = [
+  { name: "firstName", label: "Imię", required: true },
+  { name: "lastName", label: "Nazwisko", required: true },
+  { name: "street", label: "Ulica", required: true },
+  {
+    name: "houseNumber",
+    label: "Numer budynku",
+    type: "number",
+    required: true,
+  },
+  { name: "flatNumber", label: "Numer mieszkania", type: "number" },
+  {
+    name: "zip",
+    label: "Kod pocztowy",
+    pattern: "^[0-9]{2}-[0-9]{3}$",
+    required: true,
+  },
+  { name: "city", label: "Miejscowość", required: true },
+  { name: "voivodeship", label: "Województwo", required: true },
+];
 
 function handleSubmit(e) {
   e.preventDefault();
-  const firstName = form.elements[0].value;
-  const lastName = form.elements[1].value;
-  const street = form.elements[2].value;
-  const houseNumber = form.elements[3].value;
-  const zipCode = form.elements[5].value;
-  const city = form.elements[6].value;
-  const voivodeship = form.elements[7].value;
 
-  const error = [];
-  const regex = new RegExp("^[0-9]{2}-[0-9]{3}$");
+  const errors = [];
+  fields.forEach(function (field) {
+    const value = form.elements[field.name].value;
+    if (field.required) {
+      if (value.length === 0) {
+        errors.push(`${field.label} is required`);
+      }
+    }
+    if (field.type === "number") {
+      if (Number.isNaN(Number(value))) {
+        errors.push(`${field.label} must be a number`);
+      }
+    }
 
-  if (firstName.trim() === "") {
-    showMessage("first name is required!!!");
-    error.push(firstName);
-  }
-  if (lastName.trim() === "") {
-    showMessage("last name is required !!!");
-    error.push(lastName);
-  }
-  if (street.trim() === "") {
-    showMessage("street is required!!!");
-    error.push(street);
-  }
-  if (houseNumber.trim() === "") {
-    showMessage("house number is required!!!");
-    error.push(houseNumber);
-  }
-  if (regex.test(zipCode) === false) {
-    showMessage("zip code is not valid!!!");
-    error.push(zipCode);
-  }
-  if (city.trim() === "") {
-    showMessage("city is required!!!");
-    error.push(city);
-  }
-  if (voivodeship.trim() === "") {
-    showMessage("voivodeship is required!!!");
-    error.push(voivodeship);
-  }
-  if (error.length === 0) {
-    msgArray = [];
-    msgList.innerHTML = "";
-    console.log("done");
+    if (field.pattern) {
+      const reg = new RegExp(field.pattern);
+      if (!reg.test(value)) {
+        errors.push(`${field.label} is not valid`);
+      }
+    }
+  });
+  msgList.innerHTML = ""
+  if (errors.length === 0) {
+    alert("data is valid");
+    fields.forEach(function (el) {
+      form[el.name].value = "";
+    });
+  } else {
+    showMessage(errors);
   }
 }
 
-function showMessage(message) {
-  const msg = document.createElement("li");
-  msg.innerText = message;
-  if (!msgArray.includes(message)) {
-    msgArray.push(message);
-    msgList.appendChild(msg);
-  }
+function showMessage(errors) {
+  errors.forEach(function (text) {
+    const liMsg = document.createElement("li");
+    liMsg.innerText = text;
+    msgList.appendChild(liMsg);
+    console.log(errors)
+  });
 }
