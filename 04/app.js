e92a582b0d7b7c9fcfdae5b1a099bd7542508c9a
Maciej Necka <macieknecka@gmail.com>
2023-10-06 10:42:25 +0200

    Task done

--- 04/app.js
+++ 04/app.js
@@ -1,40 +1,50 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const boxElement = document.querySelector('.box');
-    setBoxShadow(boxElement, '#000000');
+  const boxElement = document.querySelector('.box');
+  setBoxShadow(boxElement, '#000000');
+  const colorInput = document.querySelector('input[name="color"]');
+  const opacityInput = document.querySelector('input[name="opacity"]');
+
+  colorInput.addEventListener('input', changeColor);
+  opacityInput.addEventListener('input', changeColor);
+
+  function changeColor() {
+    const opacity = opacityInput.value / 100;
+    const color = colorInput.value;
+    setBoxShadow(boxElement, color, opacity);
+  }
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
-    const colorInRGBA = `rgba(
+  const colorInRGBA = `rgba(
         ${getChannelColor(colorInHex, 'red')}, 
         ${getChannelColor(colorInHex, 'green')}, 
         ${getChannelColor(colorInHex, 'blue')}, 
         ${opacity}
     )`;
 
-    element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
+  element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
 }
-
 
 function getChannelColor(colorInHex, channelName) {
-    let start;
-    switch(channelName) {
-        case 'red':
-            start = 1;
-            break;
-        case 'green':
-            start = 3;
-            break;
-        case 'blue':
-            start = 5;
-            break;
-    }
+  let start;
+  switch (channelName) {
+    case 'red':
+      start = 1;
+      break;
+    case 'green':
+      start = 3;
+      break;
+    case 'blue':
+      start = 5;
+      break;
+  }
 
-    const channelColorHex = colorInHex.substr(start, 2);
-    const channelColorDec = parseInt(channelColorHex, 16);
+  const channelColorHex = colorInHex.substr(start, 2);
+  const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+  return channelColorDec;
 }
-
-
