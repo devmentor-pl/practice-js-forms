649d37bf4d7c01f3c5a44b1a49f505081023e13e
Kaczupinko <kaczupinko@gmail.com>
2023-10-23 06:23:10 +0100

    Finish 02

--- 02/app.js
+++ 02/app.js
@@ -5,14 +5,43 @@ document.addEventListener('DOMContentLoaded', function () {
         e.preventDefault();
 
         let errors = [];
+        clearErrors();
 
         const emailInput = document.querySelector('input[name="login"]');
         const password1Input = document.querySelector('input[name="pass1"]');
         const password2Input = document.querySelector('input[name="pass2"]');
         const termsCheckbox = document.querySelector('input[name="accept"]');
-        console.log(emailInput.value);
-        console.log(password1Input.value);
-        console.log(password2Input.value);
-        console.log(termsCheckbox.checked);
+        
+        switch (true) {
+            case !emailInput.value.includes('@'):
+                errors.push('Email needs to contain @');
+                markError(emailInput);
+                break;
+            case password1Input.value !== password2Input.value:
+            case password1Input.value.length < 6:
+                errors.push('Password needs to be at least 6 characters long and match');
+                markError(password1Input);
+                markError(password2Input);
+                break;
+            case !termsCheckbox.checked:
+                errors.push('You need to accept terms and conditions');
+                markError(termsCheckbox);
+                break;
+        }
+
+        if (errors.length === 0) {
+            console.log('done');
+        } else {
+            console.log(errors);
+        };
+
+        function markError(input) {
+            input.previousElementSibling.style.color = 'red';
+        }
+
+        function clearErrors() {
+            const labels = document.querySelectorAll('label');
+            labels.forEach(label => label.style.color = 'initial');
+        }
     });
 });
