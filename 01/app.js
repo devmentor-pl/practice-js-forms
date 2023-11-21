64d9f4ea2959db5301fb719e5e8b2601364537d6
Marcin Chechelski <marcin.chechelski@op.pl>
2023-11-22 00:43:39 +0100

    task done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,20 @@
-test;
+document.addEventListener("DOMContentLoaded", function () {
+    const formEl = document.querySelector("form");
+    formEl.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const firstName = e.target.elements.firstName.value;
+        const lastName = e.target.elements.lastName.value;
+
+        const newLi = document.createElement("li");
+
+        newLi.classList.add("user-list__person");
+        // newLi.innerHTML = firstName;
+        // newLi.innerHTML = lastName;
+
+        newLi.textContent = firstName + " " + lastName;
+
+        const ulLi = document.querySelector(".users-list");
+        ulLi.appendChild(newLi);
+    });
+});
