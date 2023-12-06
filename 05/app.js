b5c8df0d8dda38e224cd11679b810601864b3618
Kasia <k.kasia912@gmail.com>
2023-12-06 10:50:14 +0100

    complete task 05

--- 05/app.js
+++ 05/app.js
@@ -1,9 +1,10 @@
 const formEl = document.querySelector('form');
 const errorsBox = formEl.querySelector('.messages');
 formEl.noValidate = true;
-formEl && formEl.addEventListener('submit', validateData);
 
-function validateData(e) {
+formEl && formEl.addEventListener('submit', submitForm);
+
+function submitForm(e) {
   e.preventDefault();
   const errors = [];
   errors.length = 0;
@@ -11,28 +12,11 @@ function validateData(e) {
   const inputsList = [...formEl.elements];
 
   inputsList.forEach((input) => {
-    if (input.value === '') {
-      if (input.tagName !== 'SELECT' && input.type !== 'submit') {
-        errors.push(`${input.parentElement.innerText} jest puste`);
-      } else if (input.tagName === 'SELECT') {
-        errors.push(`${input.parentElement.firstChild.nodeValue} jest puste`);
-      }
-    }
-    if (input.value !== '' && input.type.number === isNaN) {
-      errors.push(`${input.parentElement.innerText} is not a number`);
-    }
+    validateForm(input, errors);
   });
 
   if (errors.length > 0) {
-    errors.forEach((error) => {
-      const errorInfo = document.createElement('li');
-      errorInfo.innerText = error;
-      errorInfo.style.color = 'red';
-      errorsBox.appendChild(errorInfo);
-    });
-
-    console.log(inputsList);
-    console.log(errors);
+    showErrors(errors);
   } else {
     alert('Formularz został wysłany!');
     inputsList.forEach((input) => {
@@ -41,4 +25,36 @@ function validateData(e) {
       }
     });
   }
+}
+
+function validateForm(input, errors) {
+  if (input.value === '') {
+    if (input.tagName !== 'SELECT' && input.type !== 'submit') {
+      errors.push(`${input.parentElement.innerText} jest puste`);
+    } else if (input.tagName === 'SELECT') {
+      errors.push(
+        `${input.parentElement.firstChild.nodeValue.trim()} jest puste`
+      );
+    }
+  }
+  if (input.value !== '' && input.type === 'number' && isNaN(input.value)) {
+    errors.push(`${input.parentElement.innerText} is not a number`);
+  }
+  if (input.name === 'zip') {
+    const zipCode = input.value;
+    const patternRegExp = input.getAttribute('pattern');
+    const regExp = new RegExp(patternRegExp);
+    if (!regExp.test(zipCode)) {
+      errors.push('Kod pocztowy jest błędny');
+    }
+  }
+}
+
+function showErrors(errors) {
+  errors.forEach((error) => {
+    const errorInfo = document.createElement('li');
+    errorInfo.innerText = error;
+    errorInfo.style.color = 'red';
+    errorsBox.appendChild(errorInfo);
+  });
 }
