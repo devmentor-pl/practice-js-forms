cc82cdbac91c0ea0cbe5ac86394aa78515d3d820
Kasia <k.kasia912@gmail.com>
2023-12-11 13:27:16 +0100

    Refactor code to make the solution more flexible.

--- 05/app.js
+++ 05/app.js
@@ -9,45 +9,75 @@ function submitForm(e) {
   const errors = [];
   errors.length = 0;
   errorsBox.innerHTML = '';
-  const inputsList = [...formEl.elements];
 
-  inputsList.forEach((input) => {
-    validateForm(input, errors);
-  });
+  const inputs = [
+    {name: 'firstName', label: 'Imię', required: true},
+    {
+      name: 'lastName',
+      label: 'Nazwisko',
+      required: true,
+    },
+    {name: 'street', label: 'Ulica', required: true},
+    {
+      name: 'houseNumber',
+      label: 'Numer budynku',
+      type: 'number',
+      required: true,
+    },
+    {name: 'flatNumber', label: 'Numer mieszkania', type: 'number'},
+    {
+      name: 'zip',
+      label: 'Kod pocztowy',
+      pattern: '^[0-9]{2}-[0-9]{3}$',
+      required: true,
+    },
+    {
+      name: 'city',
+      label: 'Miasto',
+      required: true,
+    },
+    {name: 'voivodeship', label: 'Województwo', required: true},
+  ];
+  validateForm(inputs, errors);
 
   if (errors.length > 0) {
     showErrors(errors);
   } else {
-    alert('Formularz został wysłany!');
-    inputsList.forEach((input) => {
-      if (input.type !== 'submit') {
-        input.value = '';
-      }
+    alert('Formularz został przesłany');
+    inputs.forEach((input) => {
+      formEl.elements[input.name].value = '';
     });
   }
 }
 
-function validateForm(input, errors) {
-  if (input.value === '') {
-    if (input.tagName !== 'SELECT' && input.type !== 'submit') {
-      errors.push(`${input.parentElement.innerText} jest puste`);
-    } else if (input.tagName === 'SELECT') {
-      errors.push(
-        `${input.parentElement.firstChild.nodeValue.trim()} jest puste`
-      );
+function validateForm(inputs, errors) {
+  inputs.forEach((input) => {
+    const {
+      name,
+      label,
+      required = false,
+      type = 'text',
+      pattern = null,
+    } = input;
+    const value = formEl.elements[name].value;
+    if (required) {
+      if (value.length === 0) {
+        errors.push(`Nie podano danych w polu ${label}`);
+      }
     }
-  }
-  if (input.value !== '' && input.type === 'number' && isNaN(input.value)) {
-    errors.push(`${input.parentElement.innerText} is not a number`);
-  }
-  if (input.name === 'zip') {
-    const zipCode = input.value;
-    const patternRegExp = input.getAttribute('pattern');
-    const regExp = new RegExp(patternRegExp);
-    if (!regExp.test(zipCode)) {
-      errors.push('Kod pocztowy jest błędny');
+
+    if (type === 'number') {
+      if (Number.isNaN(Number(value))) {
+        errors.push(`Dane w polu ${label} muszą być liczbą`);
+      }
     }
-  }
+    if (pattern) {
+      const regExp = new RegExp(pattern);
+      if (!regExp.test(value)) {
+        errors.push(`Dane w polu ${label} mają niepoprawny format`);
+      }
+    }
+  });
 }
 
 function showErrors(errors) {
