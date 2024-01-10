e13ed5caa00b8c7f5d8e35f15a64a319a291a418
Dawidtk <dawidtk@gmail.com>
2024-01-10 08:49:14 +0100

    function register

---
+++ 02/app.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+form.addEventListener("submit", register);
+
+function register(e) {
+    e.preventDefault();
+    const emailEl = document.querySelector('input[name="login"]');
+    const pass1El = document.querySelector('input[name="pass1"]');
+    const pass2El = document.querySelector('input[name="pass2"]');
+    const emailElValue = document.querySelector('input[name="login"]').value;
+    const pass1ElValue = document.querySelector('input[name="pass1"]').value;
+    const pass2ElValue = document.querySelector('input[name="pass2"]').value;
+    const checkedEl = document.querySelector('input[name="accept"]');
+    const labelList = document.querySelector("label");
+    const errors = [];
+}
