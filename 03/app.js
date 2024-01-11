f86e6d1acab14e6fb5e5ab33812cfa1d616fdb25
Dawidtk <dawidtk@gmail.com>
2024-01-11 10:05:56 +0100

    init

---
+++ 03/app.js
@@ -0,0 +1,15 @@
+/* const liEl = document.querySelector('.images-list__item--prototype');
+const inpFile = document.querySelector('input');
+const imgList = document.querySelector('.images-list');
+
+function 
+*/
+document.addEventListener("DOMContentLoaded, init");
+
+function init() {
+    const inpEl = document.querySelector("input");
+    if (inputEl) {
+        inputEl.addEventListener("change", changeHandl);
+    }
+}
+
