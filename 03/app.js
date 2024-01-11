4fd18d1033b6edb5264e02bb73c31574c73b8eab
Dawidtk <dawidtk@gmail.com>
2024-01-11 10:19:06 +0100

    forEach

--- 03/app.js
+++ 03/app.js
@@ -13,3 +13,15 @@ function init() {
     }
 }
 
+function changeHandl(e) {
+    const liEl = document.querySelector(".images-list__item--prototype");
+    const ulEl = document.querySelector(".images-list");
+    const filesList = Array.from(e.target.files);
+
+    filesList.forEach((file) => {
+        if (file.type.indexOf("image") >= 0) {
+            const liEl = protEl.cloneNode(true);
+            
+        }
+    })
+}
