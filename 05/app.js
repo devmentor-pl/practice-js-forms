0f916b0c95d13de953adc780c5f5eca39510c42d
Dawidtk <dawidtk@gmail.com>
2024-01-13 11:04:57 +0100

    refactoring done

--- 05/app.js
+++ 05/app.js
@@ -1,10 +1,11 @@
+//zip czyli kod pocztowy mialem problem, nie dzialal wiec zmienilem type z number na text ^^
 const fields = [
     { name: 'firstName', label: 'Imię', isRequired: true, pattern: '^[A-Za-z]{3,}$' },
     { name: 'lastName', label: 'Nazwisko', isRequired: true, pattern: '^[A-Za-z]{3,}$' },
     { name: 'street', label: 'Ulica', isRequired: true, pattern: '^[A-Za-z]{3,}$' },
     { name: 'houseNumber', label: 'Numer budynku', type: 'number', isRequired: true, pattern: '^[1-9]{1,3}' },
     { name: 'flatNumber', label: 'Numer mieszkania', type: 'number', pattern: '^[1-9]{1,3}' },
-    { name: 'zip', label: 'Kod pocztowy', type: 'number', isRequired: true, pattern: '^[0-9]{2}-[0-9]{3}$' },
+    { name: 'zip', label: 'Kod pocztowy', type: 'text', isRequired: true, pattern: '^[0-9]{2}-[0-9]{3}$' },
     { name: 'city', label: 'Miejscowość', isRequired: true },
     { name: 'voivodeship', label: 'Województwo', isRequired: true }
   ];
@@ -19,6 +20,47 @@ const fields = [
     };
 };
 
+//ENG: after refactoring. DE: nach der Uberarbeitung
+
+function isValid(e) {
+    e.preventDefault();
+    const ulErrorsList = document.querySelector('.messages');
+    ulErrorsList.innerHTML = '';
+    const errors = [];
+  
+    fields.forEach(({ name, label, type = 'text', isRequired, pattern }) => {
+      const value = form.elements[name].value;
+  
+      if (isRequired && value === '') {
+        errors.push(`${label} - This input can't be empty.`);
+      }
+  
+      if (type === 'number' && isNaN(Number(value))) {
+        errors.push(`${label} - This input has to be a number.`);
+      }
+  
+      if (pattern && !new RegExp(pattern).test(value)) {
+        errors.push(`${label} - This value must match the pattern or is not equal to the country where you are.`);
+      }
+    });
+  
+    console.log(errors);
+  
+    if (errors.length === 0) {
+      alert('Form successfully submitted!');
+      fields.forEach(({ name }) => {
+        form.elements[name].value = '';
+      });
+    } else {
+      errors.forEach((el) => {
+        const liErrorValue = document.createElement('li');
+        liErrorValue.style.color = 'red';
+        liErrorValue.textContent = el;
+        ulErrorsList.appendChild(liErrorValue);
+      });
+    }
+  }
+  
 
 /*code for testing but - its working ;-)
 
@@ -54,4 +96,5 @@ document.addEventListener('DOMContentLoaded', function () {
         return isValid;
     }
 });
+
 */
