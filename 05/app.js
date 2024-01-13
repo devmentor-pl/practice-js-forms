9c02145909a5614e6db7d591171e348cbc6fd10d
Dawidtk <dawidtk@gmail.com>
2024-01-13 10:46:30 +0100

    fields

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,20 @@
+const fields = [
+    { name: 'firstName', label: 'Imię', isRequired: true, pattern: '^[A-Za-z]{3,}$' },
+    { name: 'lastName', label: 'Nazwisko', isRequired: true, pattern: '^[A-Za-z]{3,}$' },
+    { name: 'street', label: 'Ulica', isRequired: true, pattern: '^[A-Za-z]{3,}$' },
+    { name: 'houseNumber', label: 'Numer budynku', type: 'number', isRequired: true, pattern: '^[1-9]{1,3}' },
+    { name: 'flatNumber', label: 'Numer mieszkania', type: 'number', pattern: '^[1-9]{1,3}' },
+    { name: 'zip', label: 'Kod pocztowy', type: 'number', isRequired: true, pattern: '^[0-9]{2}-[0-9]{3}$' },
+    { name: 'city', label: 'Miejscowość', isRequired: true },
+    { name: 'voivodeship', label: 'Województwo', isRequired: true }
+  ];
+  
+  const form = document.querySelector('form');
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
+/*code for testing but - its working ;-)
+
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form');
     const messages = document.querySelector('.messages');
@@ -30,3 +47,4 @@ document.addEventListener('DOMContentLoaded', function () {
         return isValid;
     }
 });
+*/
