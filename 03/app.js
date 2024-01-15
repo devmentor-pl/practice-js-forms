b84f52beecaffefbc7cd996b2227d82bc5217aca
Robert Staniszewski <robstani.tech@gmail.com>
2024-01-15 12:42:48 +0100

    Add code comments for better readability and understanding

--- 03/app.js
+++ 03/app.js
@@ -1,26 +1,32 @@
+// Znajdź elementy w drzewie DOM
 const imagesList = document.querySelector('.images-list');
 const inputEl = document.querySelector('input[type="file"]');
 const prototypeItem = document.querySelector('.images-list__item--prototype');
 
+// Nasłuchuj zmian w polu input (wybór plików)
 inputEl.addEventListener('change', function(e) {
+    // Przechwyć wszystkie wybrane pliki
     const capturedImgs = e.target.files;
 
+    // Iteruj przez każdy przechwycony plik
     for (let i = 0; i < capturedImgs.length; i++) {
         const capturedImg = capturedImgs[i];
     
-
+        // Sprawdź, czy przechwycony plik jest obrazem
         if (capturedImg && capturedImg.type.includes('image')) {
-            console.log('Przechwycono obraz', capturedImg);
             
+            // Pobierz nazwę i rozmiar pliku
             const fileName = capturedImg.name;
             const fileSize = capturedImg.size;
             const fileSizeMb = (fileSize / (1024 * 1024)).toFixed(2);
 
+            // Sklonuj prototyp i dostosuj jego zawartość
             const prototypeClone = prototypeItem.cloneNode(true);
             prototypeClone.querySelector('.images-list__item-name').textContent = fileName;
             prototypeClone.querySelector('.images-list__item-size').textContent = fileSizeMb + ' MB ';
             prototypeClone.querySelector('.images-list__item-img').src = URL.createObjectURL(capturedImg);
 
+            // Usuń klasę prototypu, dodaj klasę dla elementu listy, i dodaj do listy obrazów
             prototypeClone.classList.remove('images-list__item--prototype');
             prototypeClone.classList.add('images-list__item');
             imagesList.appendChild(prototypeClone);
@@ -28,6 +34,7 @@ inputEl.addEventListener('change', function(e) {
     }
 });
 
+// Obsługa kliknięcia na elemencie listy obrazów - przełącz klasę dla efektu powiększenia
 imagesList.addEventListener('click', function(e) {
     const clickedItem = e.target.closest('.images-list__item');
     clickedItem.classList.toggle('images-list__item-img');
