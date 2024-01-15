22ff3cb518c2a738056f0e7813a324c3efefb847
Robert Staniszewski <robstani.tech@gmail.com>
2024-01-15 12:20:32 +0100

    Add multiple img selection using a for loop

--- 03/app.js
+++ 03/app.js
@@ -3,22 +3,27 @@ const inputEl = document.querySelector('input[type="file"]');
 const prototypeItem = document.querySelector('.images-list__item--prototype');
 
 inputEl.addEventListener('change', function(e) {
-    const capturedImg = e.target.files[0];
-    if (capturedImg && capturedImg.type.includes('image')) {
-        console.log('Przechwycono obraz', capturedImg);
-        
-        const fileName = capturedImg.name;
-        const fileSize = capturedImg.size;
-        const fileSizeMb = (fileSize / (1024 * 1024)).toFixed(2);
+    const capturedImgs = e.target.files;
 
-        const prototypeClone = prototypeItem.cloneNode(true);
-        prototypeClone.querySelector('.images-list__item-name').textContent = fileName;
-        prototypeClone.querySelector('.images-list__item-size').textContent = fileSizeMb + ' MB ';
-        prototypeClone.querySelector('.images-list__item-img').src = URL.createObjectURL(capturedImg);
+    for (let i = 0; i < capturedImgs.length; i++) {
+        const capturedImg = capturedImgs[i];
+    
 
-        prototypeClone.classList.remove('images-list__item--prototype');
-        prototypeClone.classList.add('images-list__item-img');
-        imagesList.appendChild(prototypeClone);
+        if (capturedImg && capturedImg.type.includes('image')) {
+            console.log('Przechwycono obraz', capturedImg);
+            
+            const fileName = capturedImg.name;
+            const fileSize = capturedImg.size;
+            const fileSizeMb = (fileSize / (1024 * 1024)).toFixed(2);
+
+            const prototypeClone = prototypeItem.cloneNode(true);
+            prototypeClone.querySelector('.images-list__item-name').textContent = fileName;
+            prototypeClone.querySelector('.images-list__item-size').textContent = fileSizeMb + ' MB ';
+            prototypeClone.querySelector('.images-list__item-img').src = URL.createObjectURL(capturedImg);
+
+            prototypeClone.classList.remove('images-list__item--prototype');
+            prototypeClone.classList.add('images-list__item-img');
+            imagesList.appendChild(prototypeClone);
+        }
     }
-
 });
