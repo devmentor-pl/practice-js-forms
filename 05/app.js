c1aa8d136f2631111229b9459038f4e8a5c2d50c
Robert Staniszewski <robstani.tech@gmail.com>
2024-01-17 11:24:57 +0100

    Add zipcode pattern validation

--- 05/app.js
+++ 05/app.js
@@ -14,9 +14,12 @@ form.addEventListener('submit', function(event) {
         // Sprawdź, czy element nie jest typu "submit"
         if (input.type !== 'submit') {
             const fieldValue = getFieldValue(input);
+            const fieldName = input.name;
+
             if (fieldValue === '') {
-                const fieldName = input.name;
-                errorHandle(fieldName, event);
+                errorHandle(`Proszę wprowadź poprawne dane dla pola: ${fieldName}`, event);
+            } else if (fieldName === 'zip' && !validateZipCode(fieldValue)) {
+                errorHandle('Proszę wprowadź poprawny kod pocztowy (XX-XXX)', event);
             }
         }
     });
@@ -24,18 +27,22 @@ form.addEventListener('submit', function(event) {
 
 function getFieldValue(input) {
     if (input.tagName === 'SELECT') {
-        // Dla pola typu <select>, sprawdź, czy coś zostało wybrane
         return input.value;
     } else {
-        // Dla innych pól, po prostu pobierz wartość
         return input.value.trim();
     }
 }
 
-function errorHandle(fieldName, event) {
+function errorHandle(errorMessage, event) {
     event.preventDefault();
     const errorMsg = document.createElement('div');
     errorMsg.classList.add('error-msg');
-    errorMsg.textContent = `Proszę wprowadź poprawne dane dla pola: ${fieldName}`;
+    errorMsg.textContent = errorMessage;
     messagesEl.appendChild(errorMsg);
+}
+
+function validateZipCode(zipCode) {
+    // Walidacja wzoru kodu pocztowego
+    const zipCodePattern = /^[0-9]{2}-[0-9]{3}$/;
+    return zipCodePattern.test(zipCode);
 }
