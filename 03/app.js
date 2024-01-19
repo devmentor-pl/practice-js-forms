cd49c36954598af1c0fef4da4ca4217214da8de5
Kamil Lewandowski <kamlew555@gmail.com>
2024-01-19 19:36:45 +0100

    task done

---
+++ 03/app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const filesInput = document.querySelector(".files input");
+	const imagesList = document.querySelector(".images-list");
+	const prototypeItem = document.querySelector(".images-list__item--prototype");
+
+	filesInput.addEventListener("change", function (event) {
+		const files = event.target.files;
+
+		for (let i = 0; i < files.length; i++) {
+			const file = files[i];
+
+			if (file.type.includes("image")) {
+				const newItem = prototypeItem.cloneNode(true);
+				newItem.classList.remove("images-list__item--prototype");
+
+				const itemName = newItem.querySelector(".images-list__item-name");
+				itemName.textContent = file.name;
+
+				const itemImg = newItem.querySelector(".images-list__item-img");
+				itemImg.src = URL.createObjectURL(file);
+
+				const itemSize = newItem.querySelector(".images-list__item-size");
+				const fileSizeMB = file.size / (1024 * 1024); // Convert size to MB
+				itemSize.textContent = fileSizeMB.toFixed(2) + " MB";
+
+				imagesList.appendChild(newItem);
+			}
+		}
+	});
+});
