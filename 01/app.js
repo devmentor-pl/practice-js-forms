e27bb3d68d738b683802dd92c35cada1db28a280
Kamil Lewandowski <kamlew555@gmail.com>
2024-01-22 14:12:37 +0100

    add if statement

--- 01/app.js
+++ 01/app.js
@@ -1,22 +1,24 @@
-const formEl = document.querySelector('form')
-const firstNameInputEl = document.querySelector('input[ name="firstName"]')
-const lastNameInputEl = document.querySelector('input[ name="lastName"]')
-const userList = document.querySelector('.users-list')
-
+const formEl = document.querySelector("form");
+const firstNameInputEl = document.querySelector('input[ name="firstName"]');
+const lastNameInputEl = document.querySelector('input[ name="lastName"]');
+const userList = document.querySelector(".users-list");
 
 //listener
-formEl.addEventListener('submit', (e)=> {
-    e.preventDefault()
+formEl.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-    const firstName = firstNameInputEl.value
-    const lastName = lastNameInputEl.value
+	const firstName = firstNameInputEl.value;
+	const lastName = lastNameInputEl.value;
     
-    const newUser = document.createElement('li')
-    newUser.className = "users-list__person";
-    
-    newUser.textContent =`${firstName} ${lastName}`
-    userList.appendChild(newUser)
+	if (firstName === "" || lastName === "") {
+		alert("Uzupełnij pola formularza");
+	}
 
-    formEl.reset()
-})
+	const newUser = document.createElement("li");
+	newUser.className = "users-list__person";
 
+	newUser.textContent = `${firstName} ${lastName}`;
+	userList.appendChild(newUser);
+
+	formEl.reset();
+});
