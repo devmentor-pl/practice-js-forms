f504fd80b9b9c23aca84e92a5d552bf40dfd0335
Kamil Lewandowski <kamlew555@gmail.com>
2024-01-22 14:40:28 +0100

    modify task

--- 03/app.js
+++ 03/app.js
@@ -10,21 +10,25 @@ document.addEventListener("DOMContentLoaded", function () {
 			const file = files[i];
 
 			if (file.type.includes("image")) {
-				const newItem = prototypeItem.cloneNode(true);
-				newItem.classList.remove("images-list__item--prototype");
-
-				const itemName = newItem.querySelector(".images-list__item-name");
-				itemName.textContent = file.name;
-
-				const itemImg = newItem.querySelector(".images-list__item-img");
-				itemImg.src = URL.createObjectURL(file);
-
-				const itemSize = newItem.querySelector(".images-list__item-size");
-				const fileSizeMB = file.size / (1024 * 1024); // Convert size to MB
-				itemSize.textContent = fileSizeMB.toFixed(2) + " MB";
-
-				imagesList.appendChild(newItem);
+				createImage();
 			}
 		}
 	});
+
+	function createImage() {
+		const newItem = prototypeItem.cloneNode(true);
+		newItem.classList.remove("images-list__item--prototype");
+
+		const itemName = newItem.querySelector(".images-list__item-name");
+		itemName.textContent = file.name;
+
+		const itemImg = newItem.querySelector(".images-list__item-img");
+		itemImg.src = URL.createObjectURL(file);
+
+		const itemSize = newItem.querySelector(".images-list__item-size");
+		const fileSizeMB = file.size / (1024 * 1024);
+		itemSize.textContent = fileSizeMB.toFixed(2) + " MB";
+
+		imagesList.appendChild(newItem);
+	}
 });
