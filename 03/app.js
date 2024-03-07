3bdb18a0d8aa6c347e255ff5667de618cd10adc3
Patryk <patryk.13@o2.pl>
2024-03-07 18:46:20 +0100

    Fix 03

--- 03/app.js
+++ 03/app.js
@@ -1,9 +1,29 @@
 const imagesList = document.querySelector('.images-list');
-const inputEl = document.querySelector('input[type="file"]');
+const inputEl = document.querySelector('input[type="file"]');	
+const prototypeItem = document.querySelector('.images-list__item--prototype');	
 
 inputEl.addEventListener('change', function(e) {
-    const capturedImg = e.target.files[0];
-    if (capturedImg && capturedImg.type.includes('image')) {
-        console.log('Przechwycono obraz', capturedImg);
+    const capturedImgs = e.target.files;
+
+    for (let i = 0; i < capturedImgs.length; i++) {
+        const capturedImg = capturedImgs[i];
+
+        if (capturedImg && capturedImg.type.includes('image')){
+            const fileName = capturedImg.name;
+            const fileSize = capturedImg.size;
+            const fileSizeMb = (fileSize / (1024 * 1024)).toFixed(2);
+            const prototypeClone = prototypeItem.cloneNode(true);
+            prototypeClone.querySelector('.images-list__item-name').textContent = fileName;
+            prototypeClone.querySelector('.images-list__item-size').textContent = fileSizeMb + ' MB ';
+            prototypeClone.querySelector('.images-list__item-img').src = URL.createObjectURL(capturedImg);
+            prototypeClone.classList.remove('images-list__item--prototype');
+            prototypeClone.classList.add('images-list__item');
+            imagesList.appendChild(prototypeClone);
+        }
     }
+})
+
+imagesList.addEventListener('click', function(e) {
+    const clickedItem = e.target.closest('.images-list__item');	   
+    clickedItem.classList.toggle('images-list__item-img');	   
 });
