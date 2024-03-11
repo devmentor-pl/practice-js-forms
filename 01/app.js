8194a4aadbf0769611a5365be708e5a5677c9c3c
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-03-11 16:27:19 +0100

    01 completed

---
+++ 01/app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('form'); //ok
+form.addEventListener('submit', function(e) {
+    //zatrzymywanie wysyłki formularza
+    e.preventDefault();
+    
+    //pobieranie danych
+    const firstName = e.target.elements.firstName;
+    const lastName = e.target.elements.lastName;
+    console.log(firstName.value, lastName.value);
+
+    //dodawanie danych do <li>
+    const ulElement = document.querySelector('ul');
+    let liElement = document.createElement('li');
+    liElement.innerText= firstName.value + " " + lastName.value;
+    ulElement.appendChild(liElement);
+
+    console.log(form);
+})
