5beb36ebd8a9154df8530749db1c4a375c8d5177
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-03-16 21:15:35 +0100

    03 completed

--- 03/app.js
+++ 03/app.js
@@ -1,10 +1,22 @@
-//nasłuchiwanie do inputa "change"
-// tablica - pliki wpisane
-const input = document.querySelector('input');
-console.log(input);
-const fileList = input.type.includes('image');
-console.log(fileList);
+//DZIAŁA
+const input = document.querySelector('input'); //ok
 
 const newItem = document.querySelector('.images-list__item--prototype').cloneNode(true);
-console.log(newItem); //ok
-newItem.classList.remove()
+newItem.style.display = 'block';
+
+//nasłuchiwanie na 'change'
+input.addEventListener('change', function(e) {
+    const file = e.target.value;
+    // console.log(e.target.value);
+    
+    // dodaję do tablicy pobrane pliki
+    const fileList = [];
+    const fileDownloaded = file;
+    fileList.push(fileDownloaded); //ok
+    
+    //dodaję pobrane pliki do newItem
+    fileList.forEach(function (file) {
+        newItem.setAttribute('src', file);
+    })
+    console.log(newItem); //ok
+})
