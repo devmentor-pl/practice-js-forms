be438f58eb84c164a5922e52d5ad906a5325fd14
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-03-19 22:19:12 +0100

    all completed

--- 05/app.js
+++ 05/app.js
@@ -1,15 +1,15 @@
 //BŁĄD
 
-const form = document.querySelector('form').noValidate;
+const form = document.querySelector('form');
+form.noValidate = true;
+console.log(form);
 
 const messages = document.querySelector('.messages');
 const zipType = document.querySelector('[name="zip"]');
 
-//BŁĄD // form.addEvent... is not a function
 form.addEventListener('submit', function(e) {
     const userInput = e.target.value;
     if(!checkPostalCode() && !checkIsFilled()) { 
-        console.log('ok');
         const newLi = document.createElement('li');
         newLi.innerText = userInput;
         messages.appendChild(newLi);
@@ -20,12 +20,14 @@ form.addEventListener('submit', function(e) {
     } 
 })
 
-const patternValue = zipType.getAttribute('pattern');
-
+//BŁĄD
 function checkPostalCode(value) {
+    const patternValue = zipType.getAttribute('pattern');
     //sprawdzam czy zip jest zgodny z patternem
     const regex = new RegExp(patternValue);
     const result = regex.test(value); //true
+    console.log(result);
+    return result;
 }
 
 
@@ -36,7 +38,8 @@ function checkIsFilled() {
     const streetValue = document.querySelector('[name="street"]').value;
     const houseNumberValue = document.querySelector('[name="houseNumber"]').value;
     const cityValue = document.querySelector('[name="city"]').value;
-    if(firstNameValue && lastNameValue && streetValue && houseNumberValue && cityValue) {
+    const voivodeshipValue = document.querySelector('[name="voivodeship"]').value;
+    if(firstNameValue && lastNameValue && streetValue && houseNumberValue && cityValue && voivodeshipValue) {
         return true;
     } else {
         return false;
