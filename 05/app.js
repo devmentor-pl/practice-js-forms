6d7eee3269a3e311bcadb31cbcfaf3f59623f257
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-03-23 20:25:22 +0100

    all completed

--- 05/app.js
+++ 05/app.js
@@ -1,27 +1,30 @@
-//BŁĄD
-
 const form = document.querySelector('form');
 form.noValidate = true;
 console.log(form);
 
 const messages = document.querySelector('.messages');
 const zipType = document.querySelector('[name="zip"]');
+console.log(zipType);
 
 form.addEventListener('submit', function(e) {
+    e.preventDefault();
     const userInput = e.target.value;
-    if(!checkPostalCode() && !checkIsFilled()) { 
+    // console.log(e.target.value);
+    // console.log(checkPostalCode());
+    console.log(checkIsFilled());
+    
+    if(checkPostalCode() && checkIsFilled()) { 
+        alert('Formularz został wypełniony poprawnie'); 
+    } else {
         const newLi = document.createElement('li');
         newLi.innerText = userInput;
         messages.appendChild(newLi);
-        console.log(messages);
+        // console.log(messages);
         alert('Formularz NIE został wypełniony poprawnie');
-    } else {
-        alert('Formularz został wypełniony poprawnie'); //ok
     } 
 })
 
-//BŁĄD
-function checkPostalCode(value) {
+function checkPostalCode(value) { 
     const patternValue = zipType.getAttribute('pattern');
     //sprawdzam czy zip jest zgodny z patternem
     const regex = new RegExp(patternValue);
@@ -30,15 +33,15 @@ function checkPostalCode(value) {
     return result;
 }
 
-
-//sprawdzam czy jest wypełnione pole
+//sprawdzam czy jest wypełnione pole //OK
 function checkIsFilled() {
-    const firstNameValue = document.querySelector('[name="firstName"]').value;
-    const lastNameValue = document.querySelector('[name="lastName"]').value;
-    const streetValue = document.querySelector('[name="street"]').value;
-    const houseNumberValue = document.querySelector('[name="houseNumber"]').value;
-    const cityValue = document.querySelector('[name="city"]').value;
-    const voivodeshipValue = document.querySelector('[name="voivodeship"]').value;
+    const firstNameValue = form.querySelector('[name="firstName"]').value;
+    const lastNameValue = form.querySelector('[name="lastName"]').value;
+    const streetValue = form.querySelector('[name="street"]').value;
+    const houseNumberValue = form.querySelector('[name="houseNumber"]').value;
+    const cityValue = form.querySelector('[name="city"]').value;
+    const voivodeshipValue = form.querySelector('[name="voivodeship"]').value;
+    
     if(firstNameValue && lastNameValue && streetValue && houseNumberValue && cityValue && voivodeshipValue) {
         return true;
     } else {
