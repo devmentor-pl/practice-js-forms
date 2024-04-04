9845b70a1443c74f7acb6bed378f3e454a63b907
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-04-04 13:41:57 +0200

    05 done

--- 05/app.js
+++ 05/app.js
@@ -1,34 +1,36 @@
-//BŁĄD  w sprawdzaniu kodu zgodnego z patternem (było ok i przestało działać)
 const form = document.querySelector('form');
 form.noValidate = true;
-console.log(form);
+// console.log(form);
 
 const messages = document.querySelector('.messages');
 const zipType = document.querySelector('[name="zip"]');
-console.log(zipType);
+// console.log(zipType);
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    const userInput = e.target.value;
-    // console.log(e.target.value);
-    // console.log(checkPostalCode());
+    // const userInput = e.target.value;  
+    // do modyfikacji - warunki + message na dole
+    const userPostalCode = e.target.elements.zip.value;
+    console.log(checkPostalCode(userPostalCode));
     console.log(checkIsFilled());
     
-    if(checkPostalCode() && checkIsFilled()) { 
+    if(checkPostalCode(e.target.elements.zip.value) && checkIsFilled()) { 
         alert('Formularz został wypełniony poprawnie'); 
     } else {
-        const newLi = document.createElement('li');
-        newLi.innerText = userInput;
-        messages.appendChild(newLi);
-        // console.log(messages);
+        // const newLi = document.createElement('li');
+        // newLi.innerText = userInput;
+        // messages.appendChild(newLi);
+        // // console.log(messages);
         alert('Formularz NIE został wypełniony poprawnie');
     } 
 })
 
 function checkPostalCode(value) { 
+    console.log(value);
     const patternValue = zipType.getAttribute('pattern');
     //sprawdzam czy zip jest zgodny z patternem
     const regex = new RegExp(patternValue);
+    console.log(patternValue);
     const result = regex.test(value); //true
     console.log(result);
     return result;
@@ -43,9 +45,43 @@ function checkIsFilled() {
     const cityValue = form.querySelector('[name="city"]').value;
     const voivodeshipValue = form.querySelector('[name="voivodeship"]').value;
     
-    if(firstNameValue && lastNameValue && streetValue && houseNumberValue && cityValue && voivodeshipValue) {
+    // if(firstNameValue && lastNameValue && streetValue && houseNumberValue && cityValue && voivodeshipValue) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    const arr = [];
+    if(firstNameValue.length === 0) {
+        arr.push('Wypełnij pole imię');
+    } 
+    if (lastNameValue.length === 0) {
+        arr.push('Wypełnij pole nazwisko');
+    } 
+    if (streetValue.length === 0) {
+        arr.push('Wypełnij pole ulica');
+    } 
+    if (houseNumberValue.length === 0) {
+        arr.push('Wypełnij pole numer domu');
+    } 
+    if (cityValue.length === 0) {
+        arr.push('Wypełnij pole miasto');
+    } 
+    if (voivodeshipValue.length === 0) {
+        arr.push('Wypełnij pole województwo');
+    } 
+    
+    arr.forEach(function(value) {
+        const newLi = document.createElement('li');
+        newLi.innerText = value;
+        console.log(messages);
+        messages.appendChild(newLi);
+    }) 
+
+
+    if (arr.length === 0) {
         return true;
     } else {
         return false;
     }
+
 }
