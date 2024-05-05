d4485c0ec8bc37d3a0f70ee41450bd015a59f1ec
Jarek Stelmach <j.stelmach@onet.eu>
2024-05-05 18:31:58 +0200

    Changed first part in ex 2 in JS Forms

--- 02/app.js
+++ 02/app.js
@@ -1,18 +1,56 @@
 const formEl = document.querySelector("form")
-// formEl.addEventListener("submit", getFormData)
 const inputEmailEl = document.querySelector("#formLogin")
 const labelOfEmail = inputEmailEl.previousElementSibling
 
-inputEmailEl.addEventListener("input", checkEmail)
+formEl.addEventListener("submit", checkData)
 
-function checkEmail(e) {
-	e.preventDefault()
-	const self = e.target
-	const email = self.value
+
+function checkData(e) {
+    e.preventDefault()
+    const email = e.target.elements.login.value
+    const password1 = e.target.elements.pass1.value
+    const password2 = e.target.elements.pass2.value
+
+    const errors = []
+
     if(email.includes('@')) {
         console.log('Done');
-        labelOfEmail.style.color = ''
     } else {
+        errors.push('Email needs @ sign')
+        const labelOfEmail = e.target.elements.login.previousElementSibling
         labelOfEmail.style.color = 'red'
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// inputEmailEl.addEventListener("input", checkEmail)
+
+// function checkEmail(e) {
+// 	e.preventDefault()
+// 	const self = e.target
+// 	const email = self.value
+//     if(email.includes('@')) {
+//         console.log('Done');
+//         labelOfEmail.style.color = ''
+//     } else {
+//         labelOfEmail.style.color = 'red'
+//     }
+// }
