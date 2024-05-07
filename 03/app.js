90153a4f42510c145765e0bae86c6c7353daed24
Jarek Stelmach <j.stelmach@onet.eu>
2024-05-07 11:37:45 +0200

    Finished ex3 in JS Forms

--- 03/app.js
+++ 03/app.js
@@ -1,44 +1,44 @@
-const fileEl = document.querySelector('input')
-const imagesList = document.querySelector('.images-list')
+const fileEl = document.querySelector("input")
+const imagesList = document.querySelector(".images-list")
 
-fileEl.addEventListener('change', showImage)
+fileEl.addEventListener("change", showImage)
 
 function showImage(e) {
-    const file = e.target.files[0]
-    
-    if(file && file.type.includes('image')) {
-        const reader = new FileReader()
+	const files = e.target.files
 
-        reader.onload = function(readerEvent) {
-                        
-            const newLiForImg = document.createElement('li')
-            newLiForImg.classList.add('images-list__item')
-            
-            const newHeaderforImg = document.createElement('header')
-            newHeaderforImg.classList.add('images-list__item-name')
-            newHeaderforImg.innerText = file.name
-            newLiForImg.appendChild(newHeaderforImg)
-            
-            const newImg = document.createElement('img')
-            newImg.classList.add('images-list__item-img')
-            newImg.src = readerEvent.target.result
-            newLiForImg.appendChild(newImg)
-            
-            const newImgFooter = document.createElement('footer')
-            newImgFooter.classList.add('images-list__item-size')
-            newImgFooter.innerText = file.size
-            newLiForImg.appendChild(newImgFooter)
+	for (let i = 0; i < files.length; i++) {
+        const file = files[i]
+        console.log(file);
 
-            imagesList.appendChild(newLiForImg)
+		if (file && file.type.includes("image")) {
+			const reader = new FileReader()
 
+			reader.onload = function (readerEvent) {
+				const newLiForImg = document.createElement("li")
+				newLiForImg.classList.add("images-list__item")
 
-        }
-        reader.readAsDataURL(file)
-    }
+				const newHeaderforImg = document.createElement("header")
+				newHeaderforImg.classList.add("images-list__item-name")
+				newHeaderforImg.innerText = file.name
+				newLiForImg.appendChild(newHeaderforImg)
 
+				const newImg = document.createElement("img")
+				newImg.classList.add("images-list__item-img")
+				newImg.src = readerEvent.target.result
+				newLiForImg.appendChild(newImg)
 
-    // console.log("Nazwa pliku", file.name)
-    // console.log('Rozmiar pliku', file.size)
-    // console.log('Typ pliku', file.type);
+				const newImgFooter = document.createElement("footer")
+				newImgFooter.classList.add("images-list__item-size")
+				newImgFooter.innerText = (file.size / (1024 * 1024)).toFixed(2)
+				newLiForImg.appendChild(newImgFooter)
+
+				imagesList.appendChild(newLiForImg)
+			}
+			reader.readAsDataURL(file)
+		}
+	}
+
+	// console.log("Nazwa pliku", file.name)
+	// console.log('Rozmiar pliku', file.size)
+	// console.log('Typ pliku', file.type);
 }
-
