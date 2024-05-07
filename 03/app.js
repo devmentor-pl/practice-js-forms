c25dc1f64644a6d5a57963066ff350197a3a598d
Jarek Stelmach <j.stelmach@onet.eu>
2024-05-07 11:12:55 +0200

    Only one thing to end ex3

--- 03/app.js
+++ 03/app.js
@@ -5,7 +5,40 @@ fileEl.addEventListener('change', showImage)
 
 function showImage(e) {
     const file = e.target.files[0]
+    
+    if(file && file.type.includes('image')) {
+        const reader = new FileReader()
 
-    console.log(file.name, file.size, file.type);
+        reader.onload = function(readerEvent) {
+                        
+            const newLiForImg = document.createElement('li')
+            newLiForImg.classList.add('images-list__item')
+            
+            const newHeaderforImg = document.createElement('header')
+            newHeaderforImg.classList.add('images-list__item-name')
+            newHeaderforImg.innerText = file.name
+            newLiForImg.appendChild(newHeaderforImg)
+            
+            const newImg = document.createElement('img')
+            newImg.classList.add('images-list__item-img')
+            newImg.src = readerEvent.target.result
+            newLiForImg.appendChild(newImg)
+            
+            const newImgFooter = document.createElement('footer')
+            newImgFooter.classList.add('images-list__item-size')
+            newImgFooter.innerText = file.size
+            newLiForImg.appendChild(newImgFooter)
+
+            imagesList.appendChild(newLiForImg)
+
+
+        }
+        reader.readAsDataURL(file)
+    }
+
+
+    // console.log("Nazwa pliku", file.name)
+    // console.log('Rozmiar pliku', file.size)
+    // console.log('Typ pliku', file.type);
 }
 
