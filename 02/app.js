ad6bcc396b2946071179ca9f8716ba415187ba9c
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-05-28 17:16:05 +0200

    Task 02 finished

--- 02/app.js
+++ 02/app.js
@@ -1,21 +1,56 @@
-const form = document.querySelector('form');
+document.addEventListener('DOMContentLoaded', init)
 
-form.addEventListener('submit', validateForm);
+function init() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', validateForm);
+    let errors = [];
 
-function validateForm(e) {
-    e.preventDeafult();
-
-    const errors = [];
-    const email = e.target.elements.email.value;
-    if(!email.includes('@')) {
-        errors.push('email');
-    }
-    const pass1 = e.target.elements.pass1.value;
-    const pass2 = e.target.elements.pass2.value;
-
-    if(pass1 !== pass2) {
-        errors.push('password')
+    function showErrors(errors) {
+        errors.forEach(function (element) {
+            const label = element.previousElementSibling
+            label.style.color = 'red';
+        })
     }
 
-    console.log(errors)
+    function resetErrors(errors) {
+        errors.forEach(function (element) {
+            const label = element.previousElementSibling
+            label.style.color = 'black';  
+        })
+    }
+    
+    function validateForm(e) {
+        e.preventDefault();
+        resetErrors(errors);
+        errors = [];
+
+        const login = form.elements.login;
+        const pass1 = form.elements.pass1;
+        const pass2 = form.elements.pass2;
+        const accept = form.elements.accept;
+
+        if (!login.value.includes('@')) {
+            errors.push(login);
+        } 
+        if (pass1.value !== pass2.value) {
+            errors.push(pass2)
+        } 
+
+        if (pass1.value.length < 6) {
+            errors.push(pass1);
+            errors.push(pass2);
+        } 
+        if (!accept.checked) {
+            errors.push(accept)
+        } 
+
+        if (errors.length > 0) {
+            showErrors(errors);
+        } else {
+            console.log('done')
+        }
+    }
+
+
 }
+
