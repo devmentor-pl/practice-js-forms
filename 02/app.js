b4bfbe82712f83fa22c1e4aa7e511f5c52fea5cd
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-08 14:48:05 +0200

    deleted comment

--- 02/app.js
+++ 02/app.js
@@ -61,16 +61,4 @@ function markErrors() {
             input.parentElement.style.color = "";
         }
     });
-}
-
-
-// Tym razem nasz formularz to panel rejestracji do serwisu.
-
-// Aby można było się zarejestrować, należy:
-// - wprowadzić poprawny adres e-mail (sprawdź, czy adres zawiera znak @ lub użyj odpowiedniego [wyrażenia regularnego](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions)),
-// - podać dwa razy takie samo hasło (dla pewności, że użytkownik się nie pomylił), które jest dłuższe niż 6 znaków,
-// - zaakceptować regulamin (sprawdź, czy checkbox został zaznaczony: `checked`).
-
-// Jeśli warunki zostały spełnione, wyświetl w konsoli napis `done`. Jeśli nie – zaznacz na czerwono kolor fontu dla `<label>` przy błędnie wprowadzonych danych.
-
-// Do rozwiązania problemu oznaczania błędnych elementów możesz użyć tablicy, która będzie przechowywać dane, tj. `const errors = []`. W momencie wystąpienia błędu dodasz do tej tablicy selektor pola, które zostało błędnie wypełnione. Na samym końcu wykonasz pętlę (`for` lub `forEach`), która zaznaczy elementy z błędem.
+};
