239fc3b07064a0b3dabea54b5796e19976ac9028
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2024-12-29 18:17:10 +0100

    Task 04 Formatting fix

--- 04/app.js
+++ 04/app.js
@@ -1,50 +1,51 @@
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const boxElement = document.querySelector(".box");
-  setBoxShadow(boxElement, "#000000");
+    const boxElement = document.querySelector('.box');
+    setBoxShadow(boxElement, '#000000');
 
-  const colorInput = document.querySelector('input[type="color"]');
-  const rangeInput = document.querySelector('input[type="range"]');
-
-  const handleColorChange = function () {
-    const color = colorInput.value;
-    const opacity = rangeInput.value / 100;
-
-    setBoxShadow(boxElement, color, opacity);
-  };
-
-  colorInput.addEventListener("change", handleColorChange);
-  rangeInput.addEventListener("change", handleColorChange);
+    const colorInput = document.querySelector('input[type="color"]');
+    const rangeInput = document.querySelector('input[type="range"]');
+  
+    const handleColorChange = function () {
+      const color = colorInput.value;
+      const opacity = rangeInput.value / 100;
+  
+      setBoxShadow(boxElement, color, opacity);
+    };
+  
+    colorInput.addEventListener("change", handleColorChange);
+    rangeInput.addEventListener("change", handleColorChange);
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
-  const colorInRGBA = `rgba(
-        ${getChannelColor(colorInHex, "red")}, 
-        ${getChannelColor(colorInHex, "green")}, 
-        ${getChannelColor(colorInHex, "blue")}, 
+    const colorInRGBA = `rgba(
+        ${getChannelColor(colorInHex, 'red')}, 
+        ${getChannelColor(colorInHex, 'green')}, 
+        ${getChannelColor(colorInHex, 'blue')}, 
         ${opacity}
     )`;
 
-  element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
+    element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
 }
 
+
 function getChannelColor(colorInHex, channelName) {
-  let start;
-  switch (channelName) {
-    case "red":
-      start = 1;
-      break;
-    case "green":
-      start = 3;
-      break;
-    case "blue":
-      start = 5;
-      break;
-  }
+    let start;
+    switch(channelName) {
+        case 'red':
+            start = 1;
+            break;
+        case 'green':
+            start = 3;
+            break;
+        case 'blue':
+            start = 5;
+            break;
+    }
 
-  const channelColorHex = colorInHex.substr(start, 2);
-  const channelColorDec = parseInt(channelColorHex, 16);
+    const channelColorHex = colorInHex.substr(start, 2);
+    const channelColorDec = parseInt(channelColorHex, 16);
 
-  return channelColorDec;
+    return channelColorDec; 
 }
