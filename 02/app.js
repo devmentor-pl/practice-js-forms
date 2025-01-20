ed1b3b5db4356dcfaa0a290ed939e048d57e227c
marrcelp <marcel.piaszczyk@gmail.com>
2025-01-20 17:00:45 +0100

    refactored solution - add validation function, errors and errorMessages arrays

--- 02/app.js
+++ 02/app.js
@@ -8,33 +8,65 @@ const inputPass2 = document.querySelector('#formPass2');
 const labelForRegulations = document.querySelector('label[for="formAccept"]');
 const checkbox = document.querySelector('#formAccept');
 
+const validation = function(condition, label, errorMessage, element){
+    label.style.color = condition ? 'black' : 'red';
+
+    if(!condition){
+        errors.push(element);
+        errorMessages.push(errorMessage);
+    }
+    return condition;
+}
+
+const errors = [];
+const errorMessages = [];
+
 const handleSubmit = function(e){
     e.preventDefault();
 
-    if (!inputLogin.value.includes('@')){
-        labelForLogin.style.color = 'red';
-    } else {
-        labelForLogin.style.color = 'black';
+    // if (!inputLogin.value.includes('@')){
+    //     labelForLogin.style.color = 'red';
+    // } else {
+    //     labelForLogin.style.color = 'black';
+    // }
+
+    // if (inputPass1.value.length < 6){
+    //     labelForPass1.style.color = 'red';
+    // } else {
+    //     labelForPass1.style.color = 'black';
+    // }
+
+    // if (inputPass1.value !== inputPass2.value) {
+    //     labelForPass2.style.color = 'red';
+    // } else {
+    //     labelForPass2.style.color = 'black';
+    // }
+
+    // if (!checkbox.checked){
+    //     labelForRegulations.style.color = 'red';
+    // } else {
+    //     labelForRegulations.style.color = 'black';
+    // }
+
+    const validateEmail = validation(inputLogin.value.includes('@'), labelForLogin, 'Email musi zawierać @!', inputLogin);
+    const validatePass1 = validation(inputPass1.value.length >= 6, labelForPass1, 'Hasło musi się składać z minimum 6 znaków!', inputPass1);
+    const validatePass2 = validation(inputPass1.value === inputPass2.value, labelForPass2, 'Powtórzone hasło nie jest takie samo!', inputPass2);
+    const validateRegulations = validation(checkbox.checked, labelForRegulations, 'Musisz zaakceptować regulamin!', checkbox);
+
+    if (validateEmail && validatePass1 &&  validatePass2 && validateRegulations){
+        console.log('done');
     }
 
-    if (inputPass1.value.length < 6){
-        labelForPass1.style.color = 'red';
-    } else {
-        labelForPass1.style.color = 'black';
-    }
+    if (errors.length > 0){
 
-    if (inputPass1.value !== inputPass2.value) {
-        labelForPass2.style.color = 'red';
-    } else {
-        labelForPass2.style.color = 'black';
+        errors.forEach((element) => {
+            element.style.border = '1px solid red';
+        })
+        console.log('Wypelniony formularz zawiera ponizsze bledy: ')
+        errorMessages.forEach((msg) => {
+            console.log(msg);
+        })
     }
-
-    if (!checkbox.checked){
-        labelForRegulations.style.color = 'red';
-    } else {
-        labelForRegulations.style.color = 'black';
-    }
-    
 }
 
 form.addEventListener('submit', handleSubmit);
