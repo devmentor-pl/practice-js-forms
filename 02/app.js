118a6210ca3b58076f2c1accdd7fa7b13bd5546a
marrcelp <marcel.piaszczyk@gmail.com>
2025-01-29 14:50:18 +0100

    change comment - basic solution below

--- 02/app.js
+++ 02/app.js
@@ -1,53 +1,3 @@
-// const form = document.querySelector('form');
-// const inputLogin = document.querySelector('#formLogin');
-// const labelForLogin = document.querySelector('label[for="formLogin"]');
-// const labelForPass1 = document.querySelector('label[for="formPass1"]');
-// const labelForPass2 = document.querySelector('label[for="formPass2"]');
-// const inputPass1 = document.querySelector('#formPass1');
-// const inputPass2 = document.querySelector('#formPass2');
-// const labelForRegulations = document.querySelector('label[for="formAccept"]');
-// const checkbox = document.querySelector('#formAccept');
-
-// const validation = function(condition, label, errorMessage, element){
-//     label.style.color = condition ? 'black' : 'red';
-
-//     if(!condition){
-//         errors.push(element);
-//         errorMessages.push(errorMessage);
-//     }
-//     return condition;
-// }
-
-// const errors = [];
-// const errorMessages = [];
-
-// const handleSubmit = function(e){
-//     e.preventDefault();
-
-//     const validateEmail = validation(inputLogin.value.includes('@'), labelForLogin, 'Email musi zawierać @!', inputLogin);
-//     const validatePass1 = validation(inputPass1.value.length >= 6, labelForPass1, 'Hasło musi się składać z minimum 6 znaków!', inputPass1);
-//     const validatePass2 = validation(inputPass1.value === inputPass2.value, labelForPass2, 'Powtórzone hasło nie jest takie samo!', inputPass2);
-//     const validateRegulations = validation(checkbox.checked, labelForRegulations, 'Musisz zaakceptować regulamin!', checkbox);
-
-//     if (validateEmail && validatePass1 &&  validatePass2 && validateRegulations){
-//         console.log('done');
-//     }
-
-//     if (errors.length > 0){
-
-//         errors.forEach((element) => {
-//             element.style.border = '1px solid red';
-//         })
-//         console.log('Wypelniony formularz zawiera ponizsze bledy: ')
-//         errorMessages.forEach((msg) => {
-//             console.log(msg);
-//         })
-//     }
-// }
-
-// form.addEventListener('submit', handleSubmit);
-
-
 const form = document.querySelector('form');
 
 if (form){
@@ -126,7 +76,7 @@ function handleSubmit(e){
 
     if (errors.length === 0){
         console.log('done');
-        
+
     } else {
         errors.forEach(({message, element, labelElement}) => {
             console.log(message);
@@ -135,3 +85,55 @@ function handleSubmit(e){
         })
     }
 }
+
+// BASIC SOLUTION:
+//***********************************************************************************
+
+// const form = document.querySelector('form');
+// const inputLogin = document.querySelector('#formLogin');
+// const labelForLogin = document.querySelector('label[for="formLogin"]');
+// const labelForPass1 = document.querySelector('label[for="formPass1"]');
+// const labelForPass2 = document.querySelector('label[for="formPass2"]');
+// const inputPass1 = document.querySelector('#formPass1');
+// const inputPass2 = document.querySelector('#formPass2');
+// const labelForRegulations = document.querySelector('label[for="formAccept"]');
+// const checkbox = document.querySelector('#formAccept');
+
+// const validation = function(condition, label, errorMessage, element){
+//     label.style.color = condition ? 'black' : 'red';
+
+//     if(!condition){
+//         errors.push(element);
+//         errorMessages.push(errorMessage);
+//     }
+//     return condition;
+// }
+
+// const errors = [];
+// const errorMessages = [];
+
+// const handleSubmit = function(e){
+//     e.preventDefault();
+
+//     const validateEmail = validation(inputLogin.value.includes('@'), labelForLogin, 'Email musi zawierać @!', inputLogin);
+//     const validatePass1 = validation(inputPass1.value.length >= 6, labelForPass1, 'Hasło musi się składać z minimum 6 znaków!', inputPass1);
+//     const validatePass2 = validation(inputPass1.value === inputPass2.value, labelForPass2, 'Powtórzone hasło nie jest takie samo!', inputPass2);
+//     const validateRegulations = validation(checkbox.checked, labelForRegulations, 'Musisz zaakceptować regulamin!', checkbox);
+
+//     if (validateEmail && validatePass1 &&  validatePass2 && validateRegulations){
+//         console.log('done');
+//     }
+
+//     if (errors.length > 0){
+
+//         errors.forEach((element) => {
+//             element.style.border = '1px solid red';
+//         })
+//         console.log('Wypelniony formularz zawiera ponizsze bledy: ')
+//         errorMessages.forEach((msg) => {
+//             console.log(msg);
+//         })
+//     }
+// }
+
+// form.addEventListener('submit', handleSubmit);
