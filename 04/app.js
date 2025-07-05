8963eda44abb87c3bda26dd2ad5c3c949585702a
Zaneta Sochon <twj email>
2025-07-05 12:45:48 +0200

    feat(4): Change color and box opacity

--- 04/app.js
+++ 04/app.js
@@ -1,40 +1,53 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const boxElement = document.querySelector('.box');
-    setBoxShadow(boxElement, '#000000');
+  const boxElement = document.querySelector('.box');
+  setBoxShadow(boxElement, '#000000');
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
-    const colorInRGBA = `rgba(
+  const colorInRGBA = `rgba(
         ${getChannelColor(colorInHex, 'red')}, 
         ${getChannelColor(colorInHex, 'green')}, 
         ${getChannelColor(colorInHex, 'blue')}, 
         ${opacity}
     )`;
 
-    element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
+  element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
 }
-
 
 function getChannelColor(colorInHex, channelName) {
-    let start;
-    switch(channelName) {
-        case 'red':
-            start = 1;
-            break;
-        case 'green':
-            start = 3;
-            break;
-        case 'blue':
-            start = 5;
-            break;
-    }
+  let start;
+  switch (channelName) {
+    case 'red':
+      start = 1;
+      break;
+    case 'green':
+      start = 3;
+      break;
+    case 'blue':
+      start = 5;
+      break;
+  }
 
-    const channelColorHex = colorInHex.substr(start, 2);
-    const channelColorDec = parseInt(channelColorHex, 16);
+  const channelColorHex = colorInHex.substr(start, 2);
+  const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+  return channelColorDec;
 }
 
+const inputColorEl = document.querySelector('input[type="color"]');
+const inputRangeEl = document.querySelector('input[type="range"]');
 
+inputColorEl.addEventListener('change', function (e) {
+  const changeColor = e.target.value;
+  const rangeValue = inputRangeEl.value / 100;
+  const boxElement = document.querySelector('.box');
+  setBoxShadow(boxElement, changeColor, rangeValue);
+});
+
+inputRangeEl.addEventListener('input', function (e) {
+  const changeRange = e.target.value;
+  const boxElement = document.querySelector('.box');
+  setBoxShadow(boxElement, inputColorEl.value, changeRange / 100);
+});
