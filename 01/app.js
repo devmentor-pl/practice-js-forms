caa6fc287db2c5738eed9f4b42b332db4d2fa8c6
Daria Seńko <twoj@email>
2025-07-09 16:01:55 +0200

    01

--- 01/app.js
+++ 01/app.js
@@ -1,16 +1,21 @@
 const formEl = document.querySelector('form');
 const ulEl = document.querySelector('ul');
 
-formEl.addEventListener('submit', checkData);
+formEl.addEventListener('submit', addUser);
 
-function checkData(e) {
+function addUser(e) {
   e.preventDefault();
+
   const firstName = e.target.elements.firstName.value;
   const lastName = e.target.elements.lastName.value;
+
+  if (firstName === '' || lastName === '') {
+    alert('Proszę uzupełnić oba pola: imię i nazwisko.');
+  }
 
   const li = document.createElement('li');
   li.classList.add('user-list__person');
   li.textContent = `${firstName} ${lastName}`;
-    ulEl.appendChild(li);
-
+  ulEl.appendChild(li);
+  e.target.reset();
 }
