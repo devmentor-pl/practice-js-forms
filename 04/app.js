8ff0547afa2fe26c93b9f17f06da05933f49c469
Michal Wasiak <michal.wasiak@hotmail.com>
2025-08-23 13:42:28 +1000

    Task 04 done

--- 04/app.js
+++ 04/app.js
@@ -1,4 +1,43 @@
+// W tym zadaniu wykorzystaj inputy typu `color` oraz `range`, aby zarządzać cieniem elementu o klasie `.box`.
+
+// W pliku `app.js` masz już przygotowaną funkcję, która ustawia potrzebne style. Wystarczy, że polom formularza przypiszesz nasłuchiwanie na odpowiednie zdarzenia.
+
+// Zauważ, że nasz input typu `range` przyjmuje zakres od 0 do 100, natomiast w `rgba()` zakres dla przeźroczystości określa się w przedziale od 1 do 0.
+
+
 document.addEventListener('DOMContentLoaded', init);
+
+const colorEl = document.querySelector('input[name="color"]');
+const opacityEl = document.querySelector('input[name="opacity"]');
+colorEl.addEventListener('change', changeColor);
+opacityEl.addEventListener('change', changeColor);
+opacityEl.addEventListener('mousemove', changeColor);
+
+
+function changeColor(e) {
+    const box = document.querySelector('.box');
+    // console.log(box)
+    const color = colorEl.value;
+    // box.style.backgroundColor = color;
+    const opacity = opacityEl.value / 100;
+    setBoxShadow(box, color, opacity)
+
+    const isMouseMoveEvent = e.type === 'mousemove';
+    const isMouseLeftButtonPress = e.buttons === 1;
+    if (
+        isMouseMoveEvent && isMouseLeftButtonPress
+        || !isMouseMoveEvent
+    ) {
+        spanEl.innerText = e.target.value;
+    }
+}
+
+function changeOpacity(e) { }
+
+// console.log(colorEl)
+// console.log(opacityEl)
+
+
 
 function init() {
     const boxElement = document.querySelector('.box');
@@ -19,7 +58,7 @@ function setBoxShadow(element, colorInHex, opacity = 1) {
 
 function getChannelColor(colorInHex, channelName) {
     let start;
-    switch(channelName) {
+    switch (channelName) {
         case 'red':
             start = 1;
             break;
@@ -34,7 +73,7 @@ function getChannelColor(colorInHex, channelName) {
     const channelColorHex = colorInHex.substr(start, 2);
     const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+    return channelColorDec;
 }
 
 
